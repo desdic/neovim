84f1a29a03b7647e2c2abca87114210078aa2e9d
Kim Nielsen <kgn@one.com>
2022-01-03 13:34:39 +0100

    Add silient, noremap as options

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,88 +1,91 @@
 local keymap = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = true}
 
 -- Easy navigation
-keymap("n", "<C-h>", " <C-W>h", {})
-keymap("n", "<C-j>", " <C-W>j", {})
-keymap("n", "<C-k>", " <C-W>k", {})
-keymap("n", "<C-l>", " <C-W>l", {})
+keymap("n", "<C-h>", " <C-W>h", opts)
+keymap("n", "<C-j>", " <C-W>j", opts)
+keymap("n", "<C-k>", " <C-W>k", opts)
+keymap("n", "<C-l>", " <C-W>l", opts)
 
-keymap("n", "fn", "]m", {})
-keymap("n", "fp", "[m", {})
+keymap("n", "fn", "]m", opts)
+keymap("n", "fp", "[m", opts)
 
-keymap("n", "<C-p>", ":BufferLineCyclePrev<CR>", {})
-keymap("n", "<C-n>", ":BufferLineCycleNext<CR>", {})
+keymap("n", "<C-p>", ":BufferLineCyclePrev<CR>", opts)
+keymap("n", "<C-n>", ":BufferLineCycleNext<CR>", opts)
 
-keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true})
+keymap("t", "<Esc>", "<C-\\><C-n>", opts)
 
 -- Close, write and quit buffers
--- keymap('', '<Leader>c', ':close<CR>', {})
-keymap("", "<Leader>c", ":close<CR>", {})
-keymap("", "<Leader>w", ":w!<CR>", {})
--- keymap('n', '<Leader>q', ':BufferClose<CR>', {})
-keymap("", "<Leader>q", ":bp <BAR> bd #<CR>", {})
+-- keymap('', '<Leader>c', ':close<CR>', opts)
+keymap("", "<Leader>c", ":close<CR>", opts)
+keymap("", "<Leader>w", ":w!<CR>", opts)
+-- keymap('n', '<Leader>q', ':BufferClose<CR>', opts)
+keymap("", "<Leader>q", ":bp <BAR> bd #<CR>", opts)
 
-keymap("", "<Leader>t", ":terminal<CR>", {})
+keymap("", "<Leader>t", ":terminal<CR>", opts)
 
 -- Indent in visual mode
-keymap("v", "<", "<gv", {})
-keymap("v", ">", ">gv", {})
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
 
 -- Move with CTRL+Meta (Alt)
-keymap("n", "<C-m-j>", ":m .+1<CR>==", {noremap = true})
-keymap("n", "<C-m-k>", ":m .-2<CR>==", {noremap = true})
-keymap("i", "<C-m-j>", "<Esc>:m .+1<CR>==gi", {noremap = true})
-keymap("i", "<C-m-k>", "<Esc>:m .-2<CR>==gi", {noremap = true})
-keymap("v", "<C-m-j>", ":m '>+1<CR>gv=gv", {noremap = true})
-keymap("v", "<C-m-k>", ":m '<-2<CR>gv=gv", {noremap = true})
+keymap("n", "<C-m-j>", ":m .+1<CR>==", opts)
+keymap("n", "<C-m-k>", ":m .-2<CR>==", opts)
+keymap("i", "<C-m-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("i", "<C-m-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("v", "<C-m-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<C-m-k>", ":m '<-2<CR>gv=gv", opts)
 
-keymap("n", "<Leader>qa", ":qa!<CR>", {noremap = true})
+keymap("n", "<Leader>qa", ":qa!<CR>", opts)
 
 -- CTRL+Backspace to delete word
-keymap("i", "<C-H>", "<C-W>", {noremap = true})
+keymap("i", "<C-H>", "<C-W>", opts)
 
 -- jj as escape
-keymap("i", "jj", "<ESC>", {})
+keymap("i", "jj", "<ESC>", opts)
 
 keymap("n", "<Leader><space>", ":nohlsearch<CR>",
        {noremap = true, silent = false, expr = false})
 
 -- {{ Telescope
-keymap("n", "<Leader>a", ':lua require("telescope.builtin").marks()<CR>', {})
+keymap("n", "<Leader>a", ':lua require("telescope.builtin").marks()<CR>', opts)
 keymap("n", "<Leader>ff",
-       ':lua require("telescope.builtin").find_files({hidden=true})<CR>', {})
+       ':lua require("telescope.builtin").find_files({hidden=true})<CR>', opts)
 keymap("n", "<Leader>fv", ':lua require("telescope.builtin").treesitter()<CR>',
-       {})
+       opts)
 keymap("n", "<Leader>fm",
-       ':lua require("telescope").extensions.media_files.media_files()<CR>', {})
+       ':lua require("telescope").extensions.media_files.media_files()<CR>',
+       opts)
 keymap("n", "<Leader>fg", ':lua require("telescope.builtin").live_grep()<CR>',
-       {})
-keymap("n", "<Leader>fb", ':lua require("telescope.builtin").buffers()<CR>', {})
+       opts)
+keymap("n", "<Leader>fb", ':lua require("telescope.builtin").buffers()<CR>',
+       opts)
 keymap("n", "<Leader>fh", ':lua require("telescope.builtin").help_tags()<CR>',
-       {})
+       opts)
 keymap("n", "<Leader>fo",
        ':lua require("telescope.builtin").tags{ only_current_buffer = true }<CR>',
-       {})
+       opts)
 keymap("n", "<Leader>vrc", ':lua require("config.telescope").search_nvim()<CR>',
-       {noremap = true})
+       opts)
 keymap("n", "<Leader>notes",
-       ':lua require("config.telescope").grep_notes()<CR>', {noremap = true})
+       ':lua require("config.telescope").grep_notes()<CR>', opts)
 
-keymap("n", "<Leader>p", ":Telescope diagnostics bufnr=0<CR>", {})
-keymap("n", "<Leader>bp", ":Telescope neoclip unnamed<CR>", {})
+keymap("n", "<Leader>p", ":Telescope diagnostics bufnr=0<CR>", opts)
+keymap("n", "<Leader>bp", ":Telescope neoclip unnamed<CR>", opts)
 -- }}
 
 -- {{ Focus
-keymap("n", "<Leader>h", ":FocusSplitLeft<CR>", {silent = true})
-keymap("n", "<Leader>j", ":FocusSplitDown<CR>", {silent = true})
-keymap("n", "<Leader>k", ":FocusSplitUp<CR>", {silent = true})
-keymap("n", "<Leader>l", ":FocusSplitRight<CR>", {silent = true})
+keymap("n", "<Leader>h", ":FocusSplitLeft<CR>", opts)
+keymap("n", "<Leader>j", ":FocusSplitDown<CR>", opts)
+keymap("n", "<Leader>k", ":FocusSplitUp<CR>", opts)
+keymap("n", "<Leader>l", ":FocusSplitRight<CR>", opts)
 -- }}
 
-keymap("n", "<Leader>pack", ":PackerSync<CR>", {noremap = true})
+keymap("n", "<Leader>pack", ":PackerSync<CR>", opts)
 
 -- {{ NvimTree
-keymap("n", "<Leader>n", ":NvimTreeToggle<CR>", {noremap = true})
-keymap("n", "<Leader>r", ":NvimTreeRefresh<CR>", {noremap = true})
+keymap("n", "<Leader>n", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<Leader>r", ":NvimTreeRefresh<CR>", opts)
 -- }}
 
 vim.cmd(

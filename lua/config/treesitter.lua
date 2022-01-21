e354a2d46bd9dfbf3335c4d14fe8af04d1cf49ec
Kim Nielsen <kgn@one.com>
2022-01-21 18:21:57 +0100

    Remove deprecated iF and added a few more

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -50,18 +50,12 @@ ntconfig.setup({
             lookahead = true,
 
             keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
                 ["af"] = "@function.outer",
                 ["if"] = "@function.inner",
                 ["ac"] = "@class.outer",
                 ["ic"] = "@class.inner",
-
-                -- Or you can define your own textobjects like this
-                ["iF"] = {
-                    python = "(function_definition) @function",
-                    cpp = "(function_definition) @function",
-                    c = "(function_definition) @function"
-                }
+                ["al"] = "@loop.outer",
+                ["il"] = "@loop.inner"
             }
         }
     },

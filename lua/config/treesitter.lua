7c2e9375c5b22ffa07403ea31a8061ffe208bf0a
Kim Nielsen <kgn@one.com>
2022-01-26 07:12:17 +0100

    Playing with treesitter

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -57,6 +57,31 @@ ntconfig.setup({
                 ["al"] = "@loop.outer",
                 ["il"] = "@loop.inner"
             }
+        },
+        swap = {
+            enable = true,
+            swap_next = {["<leader>a"] = "@parameter.inner"},
+            swap_previous = {["<leader>A"] = "@parameter.inner"}
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+                ["]]"] = "@class.outer"
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+                ["]["] = "@class.outer"
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+                ["[["] = "@class.outer"
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer",
+                ["[]"] = "@class.outer"
+            }
         }
     },
     playground = {

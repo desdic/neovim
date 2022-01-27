086e024efe7ce3aca36acc18b6953b454496f5dd
Kim Nielsen <kgn@one.com>
2022-01-27 05:24:33 +0100

    Deprecated options

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -65,10 +65,10 @@ gitsigns.setup({
         ["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
         ["x ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
     },
-    watch_index = {interval = 1000},
+    watch_gitdir = {interval = 1000, follow_files = true},
     current_line_blame = false,
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
-    use_internal_diff = true -- If luajit is present
+    diff_opts = {internal = true} -- If luajit is present
 })

3184b0099a857587741a7273dd811ae070822df0
Kim Nielsen <kgn@one.com>
2022-01-28 13:34:42 +0100

    Removed unused

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -84,7 +84,6 @@ cmp.setup({
 })
 
 local keymap = vim.api.nvim_set_keymap
-local opts = {noremap = true, silent = true}
 
 keymap("i", "<Tab>", "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'",
        {expr = true})

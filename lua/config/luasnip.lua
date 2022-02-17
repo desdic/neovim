bcdc660792e51bf8fbab81e03741a61b4334d2d9
Kim Nielsen <kgn@one.com>
2022-02-17 11:12:04 +0100

    Added snippet for c/cpp safe guards
    - Better snippet for #!

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -7,10 +7,11 @@ end
 local s = ls.s
 local i = ls.insert_node
 local d = ls.dynamic_node
+local t = ls.text_node
 local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 local types = require("luasnip.util.types")
--- local t = ls.text_node
+local r = require("luasnip.extras").rep
 -- local c = ls.choice_node
 -- local rep = require("luasnip.extras").rep
 
@@ -24,33 +25,50 @@ ls.config.set_config({
     enable_autosnippets = true,
 
     ext_opts = {
-        [types.choiceNode] = {
-            active = {virt_text = {{"<--", "Error"}}}
-        }
+        [types.choiceNode] = {active = {virt_text = {{"●", "Question"}}}},
+        [types.insertNode] = {active = {virt_text = {{"●", "Question"}}}}
     }
 })
 
 local date_input = function(_, _, old_state, format)
-  if not old_state then
-    old_state = {}
-  end
+    if not old_state then old_state = {} end
 
-  local cfmt = format or '%Y-%m-%d'
-  return sn(nil, i(1, os.date(cfmt)))
+    local cfmt = format or "%Y-%m-%d"
+    return sn(nil, i(1, os.date(cfmt)))
+end
+
+local function file_begin()
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return row == 1 and col == 1
 end
 
 ls.snippets = {
     all = {
-        ls.parser.parse_snippet("$file$", "$TM_FILENAME"),
+        ls.parser.parse_snippet("$file$", "$TM_FILENAME"), s("#!", {
+            t("#!/usr/bin/env "),
+            d(1, function(_) return sn(nil, i(1, vim.bo.filetype)) end, {}),
+            t({"", ""}), i(0)
+        }, {condition = file_begin, show_condition = file_begin})
     },
 
     lua = {},
 
     python = {
-        s("main", fmt('if __name__ == "__main__":\n\t{}', i(0, "main()"))),
-        s("#!", fmt("#!/usr/bin/env {}", i(0, "python3"))),
         s("def", fmt("def {}({}) -> {}:\n\t{}\n",
                      {i(1, "name"), i(2), i(3, "None"), i(0, "pass")}))
+    },
+
+    cpp = {
+        s({trig = "#ifndef", name = "header guard"}, {
+            t("#ifndef "),
+			d(1, function(_)
+				-- Uppercase filename and replace . with _
+				local filename = "__" .. string.upper(vim.fn.expand("%"):gsub("%.", "_")) .. "__"
+				return sn(nil, i(1, filename))
+			end, {}),
+			t({"", "#define "}), r(1),
+            t({"", "", "#endif"})
+        })
     },
 
     debchangelog = {

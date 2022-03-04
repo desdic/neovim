1e7d17cf1c231c5da98a878dd7fe03b99654f634
Kim Nielsen <kgn@one.com>
2022-03-04 22:25:27 +0100

    Changed shortcuts for jumping

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -71,21 +71,27 @@ ntconfig.setup({
             enable = true,
             set_jumps = true, -- whether to set jumps in the jumplist
             goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer"
-            },
-            goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer"
+                ["[f"] = "@function.outer",
+                ["[c"] = "@class.outer",
+                ["[l"] = "@loop.outer",
+                ["[p"] = "@parameter.inner",
+                ["[i"] = "@conditional.outer",
+                ["[,"] = "@call.outer",
             },
             goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer"
+                ["]f"] = "@function.outer",
+                ["]c"] = "@class.outer",
+                ["]l"] = "@loop.outer",
+                ["]p"] = "@parameter.inner",
+                ["]i"] = "@conditional.outer",
+                ["],"] = "@call.outer",
             },
-            goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer"
-            }
+            goto_next_end = {
+                ["[F"] = "@function.outer",
+                ["[C"] = "@class.outer",
+                ["[L"] = "@loop.outer",
+                ["[P"] = "@parameter.inner",
+            },
         }
     },
     playground = {

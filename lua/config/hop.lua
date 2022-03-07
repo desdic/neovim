5999d13a0e09d40933e53a8fc64fc2ffb304e416
Kim Nielsen <kgn@one.com>
2022-03-07 07:26:38 +0100

    Changed hop keymap to s

--- lua/config/hop.lua
+++ lua/config/hop.lua
@@ -12,12 +12,10 @@ local keymap = vim.api.nvim_set_keymap
 local opts = {}
 
 keymap('n', 's', "<cmd>lua require'hop'.hint_char1()<CR>", opts)
-keymap('n', 'f', "<cmd>lua require'hop'.hint_char1()<CR>", opts)
 keymap('n', 'F', "<cmd>lua require'hop'.hint_words()<CR>", opts)
 
-keymap('o', 'f', "<cmd>lua require'hop'.hint_char1({ inclusive_jump = true })<CR>", opts)
+keymap('o', 's', "<cmd>lua require'hop'.hint_char1({ inclusive_jump = true })<CR>", opts)
 keymap('o', 'F', "<cmd>lua require'hop'.hint_words({ inclusive_jump = true })<CR>", opts)
 
-keymap('v', 'f', "<cmd>lua require'hop'.hint_words({ inclusive_jump = true })<CR>", opts)
-
+keymap('v', 's', "<cmd>lua require'hop'.hint_words({ inclusive_jump = true })<CR>", opts)
 keymap("n", "<Leader>hl", "<cmd>lua require'hop'.hint_lines()<CR>", opts)

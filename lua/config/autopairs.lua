0a36dee2355ffedfe92a2eb0da8b46caca9ab9fe
Kim Gert Nielsen <kgn@one.com>
2022-04-20 14:57:58 +0200

    Formatting

--- lua/config/autopairs.lua
+++ lua/config/autopairs.lua
@@ -1,30 +1,30 @@
 local ok, ap = pcall(require, "nvim-autopairs")
 if not ok then
-    vim.notify("Unable to require nvim-autopairs", vim.lsp.log_levels.ERROR,
-               {title = "Plugin error"})
-    return
+	vim.notify("Unable to require nvim-autopairs", vim.lsp.log_levels.ERROR,
+		{ title = "Plugin error" })
+	return
 end
 
 ap.setup({
-    check_ts = true,
-    ts_config = {lua = {"string", "source"}},
-    disable_filetype = {"TelescopePrompt"},
-    fast_wrap = {
-        map = "<M-e>",
-        chars = {"{", "[", "(", '"', "'"},
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        offset = -1, -- Offset from pattern match
-        end_key = "$",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = true,
-        highlight = "PmenuSel",
-        highlight_grey = "LineNr"
-    }
+	check_ts = true,
+	ts_config = { lua = { "string", "source" } },
+	disable_filetype = { "TelescopePrompt" },
+	fast_wrap = {
+		map = "<M-e>",
+		chars = { "{", "[", "(", '"', "'" },
+		pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+		offset = -1, -- Offset from pattern match
+		end_key = "$",
+		keys = "qwertyuiopzxcvbnmasdfghjkl",
+		check_comma = true,
+		highlight = "PmenuSel",
+		highlight_grey = "LineNr"
+	}
 })
 
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-  return
+	return
 end
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })

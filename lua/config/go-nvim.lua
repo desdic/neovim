796fc0717af2716076edfa30f1e37955e15cb87b
Kim Gert Nielsen <kgn@one.com>
2022-04-28 12:40:49 +0200

    Guard and use lua

--- lua/config/go-nvim.lua
+++ lua/config/go-nvim.lua
@@ -4,7 +4,14 @@ if not ok then
 	return
 end
 
+local okformat, goformat = pcall(require, "go.format")
+if not okformat then
+	vim.notify("Unable to require go.format", vim.lsp.log_levels.ERROR, { title = "Plugin error" })
+	return
+end
+
 go.setup({ dap_debug = true, dap_debug_gui = true })
 
 -- Run gofmt + goimport on save
-vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
+vim.api.nvim_create_autocmd({"BufWritePre"}, {pattern = "*.go", callback = function() goformat.goimport() end})
+

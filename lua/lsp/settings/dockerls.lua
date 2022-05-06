7c18720958720e427fbfc15bf0744680a455ac36
Kim Gert Nielsen <kgn@one.com>
2022-05-06 07:39:06 +0200

    Fix path to dockerls

--- lua/lsp/settings/dockerls.lua
+++ lua/lsp/settings/dockerls.lua
@@ -1,7 +1,7 @@
 return {
     cmd = {
         DATA_PATH ..
-            "/lsp_servers/dockerfile/node_modules/.bin/docker-langserver",
+            "/lsp_servers/dockerls/node_modules/dockerfile-language-server-nodejs/bin/docker-langserver",
         "--stdio"
     },
     root_dir = vim.loop.cwd

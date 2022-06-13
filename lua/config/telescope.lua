075bf0b25aac7553d2d279be50a218fd24ce4f7e
Kim Gert Nielsen <kgn@one.com>
2022-06-13 07:55:42 +0200

    Remove ignore patterns

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -22,8 +22,7 @@ end
 ts.setup({
     defaults = {
         file_ignore_patterns = {
-            ".git/", ".cache/", "%.o", "%.a", "%.out", "%.class", "%.pdf",
-            "%.mkv", "%.mp4", "%.zip"
+            ".git/", ".cache/"
         },
         prompt_prefix = " ",
         selection_caret = " ",

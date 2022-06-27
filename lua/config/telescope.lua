15a568695b3ff76fb8fa17bb36902a9cbfe9c297
Kim Gert Nielsen <kgn@one.com>
2022-06-27 08:49:11 +0200

    Skip .o files

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -22,7 +22,7 @@ end
 ts.setup({
     defaults = {
         file_ignore_patterns = {
-            ".git/", ".cache/"
+            ".git/", ".cache/", ".o$"
         },
         prompt_prefix = " ",
         selection_caret = " ",

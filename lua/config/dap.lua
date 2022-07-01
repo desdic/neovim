ab8f585313eb62ffeba28c7430f533f7ea2ff0ff
Kim Gert Nielsen <kgn@one.com>
2022-07-01 11:51:11 +0200

    Make DAP work with gdb

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -19,13 +19,50 @@ if not okpy then
     return
 end
 
-
 local okui, dapui = pcall(require, "dapui")
 if not okui then
     vim.notify("Unable to require dapui", vim.lsp.log_levels.ERROR,
 			   {title = "Plugin error"})
     return
 end
+
+-- https://github.com/microsoft/vscode-cpptools/releases/latest
+-- download cpptools-linux.vsix
+-- cd ~/software/cpptools-linux
+-- unzip ~/Downloads/cpptools-linux.vsix
+-- chmod +x extension/debugAdapters/bin/OpenDebugAD7
+dap.adapters.cppdbg = {
+    type = "executable",
+	id = 'cppdbg',
+	command = os.getenv("HOME") .. "/software/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7"
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp --Reuse for c
 
 dapgo.setup()
 dappy.setup("~/.virtualenvs/debugpy/bin/python")

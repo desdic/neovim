aa60051fab974519c16376929bb762eb58bac813
Kim Gert Nielsen <kgn@one.com>
2022-07-01 12:13:36 +0200

    Formatting

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -1,28 +1,28 @@
 local ok, dap = pcall(require, "dap")
 if not ok then
     vim.notify("Unable to require dap", vim.lsp.log_levels.ERROR,
-			   {title = "Plugin error"})
+               {title = "Plugin error"})
     return
 end
 
 local okgo, dapgo = pcall(require, "dap-go")
 if not okgo then
     vim.notify("Unable to require dap-go", vim.lsp.log_levels.ERROR,
-			   {title = "Plugin error"})
+               {title = "Plugin error"})
     return
 end
 
 local okpy, dappy = pcall(require, "dap-python")
 if not okpy then
     vim.notify("Unable to require dap-python", vim.lsp.log_levels.ERROR,
-			   {title = "Plugin error"})
+               {title = "Plugin error"})
     return
 end
 
 local okui, dapui = pcall(require, "dapui")
 if not okui then
     vim.notify("Unable to require dapui", vim.lsp.log_levels.ERROR,
-			   {title = "Plugin error"})
+               {title = "Plugin error"})
     return
 end
 
@@ -33,64 +33,67 @@ end
 -- chmod +x extension/debugAdapters/bin/OpenDebugAD7
 dap.adapters.cppdbg = {
     type = "executable",
-	id = 'cppdbg',
-	command = os.getenv("HOME") .. "/software/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7"
+    id = "cppdbg",
+    command = os.getenv("HOME") ..
+        "/software/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7"
 }
 
 dap.configurations.cpp = {
-	{
-		name = "Launch file",
-		type = "cppdbg",
-		request = "launch",
-		program = function()
-			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-		end,
-		cwd = '${workspaceFolder}',
-		stopOnEntry = true,
-		setupCommands = {
-			{
-				text = '-enable-pretty-printing',
-				description =  'enable pretty printing',
-				ignoreFailures = false
-			},
-		},
-	},
-	{
-		name = 'Attach to gdbserver :1234',
-		type = 'cppdbg',
-		request = 'launch',
-		MIMode = 'gdb',
-		miDebuggerServerAddress = 'localhost:1234',
-		miDebuggerPath = '/usr/bin/gdb',
-		cwd = '${workspaceFolder}',
-		program = function()
-			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-		end,
-	},
+    {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/",
+                                "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = true,
+        setupCommands = {
+            {
+                text = "-enable-pretty-printing",
+                description = "enable pretty printing",
+                ignoreFailures = false
+            }
+        }
+    }, {
+        name = "Attach to gdbserver :1234",
+        type = "cppdbg",
+        request = "launch",
+        MIMode = "gdb",
+        miDebuggerServerAddress = "localhost:1234",
+        miDebuggerPath = "/usr/bin/gdb",
+        cwd = "${workspaceFolder}",
+        program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/",
+                                "file")
+        end
+    }
 }
 
-dap.configurations.c = dap.configurations.cpp --Reuse for c
+dap.configurations.c = dap.configurations.cpp -- Reuse for c
 
 dapgo.setup()
 dappy.setup("~/.virtualenvs/debugpy/bin/python")
 dapui.setup()
 
-vim.fn.sign_define('DapBreakpoint', {text='🛑', texthl='', linehl='', numhl=''})
+vim.fn.sign_define("DapBreakpoint",
+                   {text = "🛑", texthl = "", linehl = "", numhl = ""})
 
 vim.keymap.set("n", "<F4>", function() dapui.toggle() end,
-	{noremap = true, desc = "Toggle debugging"})
+               {noremap = true, desc = "Toggle debugging"})
 
 vim.keymap.set("n", "<F5>", function() dap.toggle_breakpoint() end,
-	{noremap = true, desc = "Set breakpoint"})
+               {noremap = true, desc = "Set breakpoint"})
 
 vim.keymap.set("n", "<F9>", function() dap.continue() end,
-	{noremap = true, desc = "Continue or start"})
+               {noremap = true, desc = "Continue or start"})
 
 vim.keymap.set("n", "<F1>", function() dap.step_over() end,
-	{noremap = true, desc = "Step over"})
+               {noremap = true, desc = "Step over"})
 
 vim.keymap.set("n", "<F2>", function() dap.step_into() end,
-	{noremap = true, desc = "Step into"})
+               {noremap = true, desc = "Step into"})
 
 vim.keymap.set("n", "<F3>", function() dap.step_out() end,
-	{noremap = true, desc = "Step out"})
+               {noremap = true, desc = "Step out"})

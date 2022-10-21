9b5429a38108fc08d415bcac37a69b2c0b8e152c
Kim Gert Nielsen <kgn@one.com>
2022-10-21 08:06:37 +0200

    Disable it for now

--- lua/config/vim-fugitive.lua
+++ lua/config/vim-fugitive.lua
@@ -2,4 +2,4 @@ local opts = {}
 
 -- vim.keymap.set('n', 'gh', ":diffget //3<CR>", opts)
 -- vim.keymap.set('n', 'gu', ":diffget //2<CR>", opts)
-vim.keymap.set("n", "gs", ":G<CR>", opts)
+-- vim.keymap.set("n", "gs", ":G<CR>", opts)

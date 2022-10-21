a37f5c8d4c23c42505699dd91ee2bc40a3d69802
Kim Gert Nielsen <kgn@one.com>
2022-10-21 14:43:10 +0200

    Add missing border to cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -75,5 +75,8 @@ cmp.setup({
         {name = "nvim_lsp"}, {name = "nvim_lua"}, {name = "path"},
         {name = "luasnip"}, {name = "buffer", keyword_length = 3}
     },
-    window = {documentation = cmp.config.window.bordered()}
+    window = {
+		documentation = cmp.config.window.bordered(),
+		completion = cmp.config.window.bordered(),
+	},
 })

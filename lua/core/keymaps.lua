eeeae8ffc46b6e83bd4e921dd8ac4e8224387c82
Kim Gert Nielsen <kgn@one.com>
2022-10-31 09:43:15 +0100

    Add keymappings for clipboard/centering

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -43,3 +43,17 @@ vim.cmd('command! -nargs=0 LspVirtualTextToggle lua require("lsp/virtual_text").
 vim.keymap.set("n", "<Leader>cm", ":delmarks a-zA-Z0-9<CR>", { silent = true, noremap = true, desc = "Clear all marks" })
 
 -- Keymaps for plugins is located with plugin setup
+
+vim.keymap.set("n", "Y", "yg$", { silent = true, noremap = true, desc = "yank line" })
+vim.keymap.set("x", "<Leader>p", "\"_dP", { silent = true, noremap = true, desc = "paste?" })
+vim.keymap.set({"n", "v"}, "<Leader>y", "\"+y", { silent = true, noremap = true, desc = "yank to clipboard" })
+vim.keymap.set("n", "<Leader>Y", "\"+Y", { silent = true, noremap = true, desc = "yank line to clipboard" })
+
+-- The mapping xnoremap p "_dP changes the behavior of p when pasting over selected text which is at the end of a line. That occurs because when text at the end of a line is deleted, the cursor moves back to the last character on the line, and P then pastes before that last character.
+vim.keymap.set({"n", "v"}, "<Leader>d", "\"_d", { silent = true, noremap = true, desc = "delete?" })
+
+vim.keymap.set("n", "n", "nzzzv", { silent = true, noremap = true, desc = "search next and center" })
+vim.keymap.set("n", "N", "Nzzzv", { silent = true, noremap = true, desc = "search next and center" })
+vim.keymap.set("n", "J", "mzJ`z", { silent = true, noremap = true, desc = "join next line and center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true, noremap = true, desc = "jump center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true, noremap = true, desc = "jump center" })

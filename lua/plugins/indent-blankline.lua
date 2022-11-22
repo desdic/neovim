d6e6b90eb663704aa349c78bf455ec7e2f12445a
Kim Gert Nielsen <kgn@one.com>
2022-11-22 06:15:41 +0100

    Removed hop and indent-line

--- lua/plugins/indent-blankline.lua
+++
@@ -1,8 +0,0 @@
-local lsok, ib = pcall(require, "indent_blankline")
-if not lsok then
-    vim.notify("Unable to require indent_blankline", vim.lsp.log_levels.ERROR,
-               {title = "Plugin error"})
-    return
-end
-
-ib.setup()

81141f008ff1b26fb6eda0da34688654e685b888
Kim Gert Nielsen <kgn@one.com>
2023-01-01 18:49:10 +0100

    Move luasnippet, treesitter and nvim-cmp to lazy

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -4,15 +4,6 @@ return {
     "kyazdani42/nvim-web-devicons", -- font icons
     {"ray-x/guihua.lua", build = "cd lua/fzy && make"}, -- LUA gui
     "rcarriga/nvim-notify", -- notifier
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = function() require("nvim-treesitter.install").update({with_sync = true}) end
-    }, {"nvim-treesitter/nvim-treesitter-refactor", dependencies = {"nvim-treesitter"}},
-    {"nvim-treesitter/nvim-treesitter-textobjects", dependencies = {"nvim-treesitter"}},
-    {"nvim-treesitter/nvim-treesitter-context", dependencies = {"nvim-treesitter"}},
-    {"JoosepAlviste/nvim-ts-context-commentstring", dependencies = {"nvim-treesitter"}},
-    {"nvim-treesitter/playground", dependencies = {"nvim-treesitter"}},
-    {"p00f/nvim-ts-rainbow", dependencies = {"nvim-treesitter"}}, -- color scope
     "gpanders/editorconfig.nvim", -- use editorconfig
     "neovim/nvim-lspconfig", -- LSP config
     "williamboman/mason.nvim", -- installer for LSP servers
@@ -26,16 +17,6 @@ return {
             "go install golang.org/x/tools/cmd/goimports@latest"
         }
     }, {"ray-x/lsp_signature.nvim", dependencies = {"neovim/nvim-lspconfig"}}, "kshenoy/vim-signature",
-    "hrsh7th/nvim-cmp", -- The completion plugin
-    "hrsh7th/cmp-buffer", -- buffer completions
-    "hrsh7th/cmp-path", -- path completions
-    "hrsh7th/cmp-cmdline", -- cmdline completions
-    "hrsh7th/cmp-nvim-lsp", -- completion using LSP
-    "hrsh7th/cmp-nvim-lua", -- Lua completion
-    "saadparwaiz1/cmp_luasnip", -- snippet engine in lua
-    "L3MON4D3/LuaSnip", -- snippet completions
     {"glepnir/lspsaga.nvim", branch = "main"}, -- UI for diagnostics
-    "onsails/lspkind.nvim", -- pictograms for LSP
-    "rafamadriz/friendly-snippets", -- collection of snippets
     "goolord/alpha-nvim" -- start screen
 }

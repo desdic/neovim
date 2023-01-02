2b3af13cd0eb5830127c778552a567a8839716c0
Kim Gert Nielsen <kgn@one.com>
2023-01-02 10:32:12 +0100

    Don't lazyload lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,4 @@
-local M = {"nvim-lualine/lualine.nvim"}
+local M = {"nvim-lualine/lualine.nvim", lazy = false}
 
 function M.config()
     local hide_in_width = function() return vim.fn.winwidth(0) > 80 end

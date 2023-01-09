6556784486aebc875d45291fd78341455e82e83a
Kim Gert Nielsen <kgn@one.com>
2023-01-09 09:15:10 +0100

    Just return array

--- lua/plugins/inc-rename.lua
+++ lua/plugins/inc-rename.lua
@@ -1,8 +1,6 @@
-local M = {
+return {
     "smjonas/inc-rename.nvim",
     cmd = "IncRename",
     keys = {{"<Leader>rn", ":IncRename " .. vim.fn.expand("<cword>"), desc = "[R]e[n]ame"}},
     opts = {input_buffer_type = "dressing"}
 }
-
-return M

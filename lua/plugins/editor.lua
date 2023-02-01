406c3b63a4b88dcb1ee6bfaa328ec76f882d27df
Kim Gert Nielsen <kgn@one.com>
2023-02-01 16:50:23 +0100

    Cleanup harpoon

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -21,8 +21,12 @@ return {
                         target_windows = {winid},
                         targets = require("utils/lines").get_line_starts(winid)
                     })
-                    vim.schedule(function() vim.cmd([[norm o]]) end)
-                    vim.schedule(function() vim.cmd([[startinsert]]) end)
+                    vim.schedule(function()
+                        vim.cmd([[norm o]])
+                    end)
+                    vim.schedule(function()
+                        vim.cmd([[startinsert]])
+                    end)
                 end
             }
         },
@@ -34,8 +38,19 @@ return {
         "echasnovski/mini.bufremove",
         event = "VeryLazy",
         keys = {
-            {"<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "[B]uffer [d]elete"},
-            {"<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "[B]uffer [d]elete force"}
+            {
+                "<leader>bd",
+                function()
+                    require("mini.bufremove").delete(0, false)
+                end,
+                desc = "[B]uffer [d]elete"
+            }, {
+                "<leader>bD",
+                function()
+                    require("mini.bufremove").delete(0, true)
+                end,
+                desc = "[B]uffer [d]elete force"
+            }
         },
         config = function() require("mini.bufremove").setup() end
     }, {
@@ -60,11 +75,23 @@ return {
     }, {
         "AckslD/nvim-neoclip.lua",
         opts = {},
-        keys = {{"<Leader>bp", ":Telescope neoclip unnamed<CR>", desc = "Show clipboard buffers"}}
+        keys = {
+            {
+                "<Leader>bp",
+                ":Telescope neoclip unnamed<CR>",
+                desc = "Show clipboard buffers"
+            }
+        }
     }, {
         "smjonas/inc-rename.nvim",
         cmd = "IncRename",
-        keys = {{"<Leader>rn", ":IncRename " .. vim.fn.expand("<cword>"), desc = "[R]e[n]ame"}},
+        keys = {
+            {
+                "<Leader>rn",
+                ":IncRename " .. vim.fn.expand("<cword>"),
+                desc = "[R]e[n]ame"
+            }
+        },
         opts = {input_buffer_type = "dressing"}
     }, {
         "toppair/peek.nvim",
@@ -105,20 +132,44 @@ return {
                 -- direction = "horizontal",
                 close_on_exit = true,
                 shell = vim.o.shell,
-                float_opts = {border = "curved", winblend = 0, highlights = {border = "Normal", background = "Normal"}}
+                float_opts = {
+                    border = "curved",
+                    winblend = 0,
+                    highlights = {border = "Normal", background = "Normal"}
+                }
             })
 
             function _G.set_terminal_keymaps()
-                vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], {noremap = true, buffer = 0, desc = "Esc in terminal"})
-                vim.keymap.set("t", "jk", [[<C-\><C-n>]], {noremap = true, buffer = 0, desc = "Esc in terminal"})
-                vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-W>h]],
-                               {noremap = true, buffer = 0, desc = "Navigate to left window"})
-                vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-W>j]],
-                               {noremap = true, buffer = 0, desc = "Navigate to window below"})
-                vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-W>k]],
-                               {noremap = true, buffer = 0, desc = "Navigate to window above"})
-                vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-W>l]],
-                               {noremap = true, buffer = 0, desc = "Navigate to right window"})
+                vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], {
+                    noremap = true,
+                    buffer = 0,
+                    desc = "Esc in terminal"
+                })
+                vim.keymap.set("t", "jk", [[<C-\><C-n>]], {
+                    noremap = true,
+                    buffer = 0,
+                    desc = "Esc in terminal"
+                })
+                vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-W>h]], {
+                    noremap = true,
+                    buffer = 0,
+                    desc = "Navigate to left window"
+                })
+                vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-W>j]], {
+                    noremap = true,
+                    buffer = 0,
+                    desc = "Navigate to window below"
+                })
+                vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-W>k]], {
+                    noremap = true,
+                    buffer = 0,
+                    desc = "Navigate to window above"
+                })
+                vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-W>l]], {
+                    noremap = true,
+                    buffer = 0,
+                    desc = "Navigate to right window"
+                })
             end
 
             vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
@@ -126,7 +177,9 @@ return {
     }, {
         "nvim-tree/nvim-tree.lua",
         event = "VeryLazy",
-        keys = {{"<Leader>n", ":NvimTreeToggle<CR>", desc = "Start file browser"}},
+        keys = {
+            {"<Leader>n", ":NvimTreeToggle<CR>", desc = "Start file browser"}
+        },
         dependencies = {{"kyazdani42/nvim-web-devicons"}},
 
         config = function()
@@ -139,51 +192,75 @@ return {
 
             -- configure nvim-tree
             require("nvim-tree").setup({
-                renderer = {icons = {glyphs = {folder = {arrow_closed = "", arrow_open = ""}}}},
+                renderer = {
+                    icons = {
+                        glyphs = {
+                            folder = {arrow_closed = "", arrow_open = ""}
+                        }
+                    }
+                },
                 actions = {open_file = {window_picker = {enable = false}}},
                 git = {ignore = false}
             })
         end
     }, {
         "ThePrimeagen/harpoon",
-        cmd = "Harpoon",
+        event = "VeryLazy",
         opts = {},
         keys = {
-            {"<Leader>a", ":Harpoon add_file<CR>", desc = "Add file to harpoon"},
-            {"<Leader>z", ":Harpoon nav_next<CR>", desc = "Next file in harpoon"},
-            {"<Leader>x", ":Harpoon nav_prev<CR>", desc = "Previous file in harpoon"},
-            {"<Leader>1", ":Harpoon file1<CR>", desc = "file 1"}, {"<Leader>2", ":Harpoon file2<CR>", desc = "file 2"},
-            {"<Leader>3", ":Harpoon file3<CR>", desc = "file 3"}, {"<Leader>4", ":Harpoon file4<CR>", desc = "file 4"}
+            {
+                "<Leader>a",
+                function() require("harpoon.mark").add_file() end,
+                desc = "Add file to harpoon"
+            }, {
+                "<Leader>z",
+                function() require("harpoon.ui").nav_next() end,
+                desc = "Next file in harpoon"
+            }, {
+                "<Leader>x",
+                function() require("harpoon.ui").nav_prev() end,
+                desc = "Previous file in harpoon"
+            }, {
+                "<Leader>1",
+                function() require("harpoon.ui").nav_file(1) end,
+                desc = "file 1"
+            }, {
+                "<Leader>2",
+                function() require("harpoon.ui").nav_file(2) end,
+                desc = "file 2"
+            }, {
+                "<Leader>3",
+                function() require("harpoon.ui").nav_file(3) end,
+                desc = "file 3"
+            }, {
+                "<Leader>4",
+                function() require("harpoon.ui").nav_file(4) end,
+                desc = "file 4"
+            }
         },
-        config = function()
-            vim.api.nvim_create_user_command("Harpoon", function(args)
-                if args.args == "add_file" then
-                    require("harpoon.mark").add_file()
-                elseif args.args == "nav_next" then
-                    require("harpoon.ui").nav_next()
-                elseif args.args == "nav_prev" then
-                    require("harpoon.ui").nav_prev()
-                elseif args.args == "nav_prev" then
-                    require("harpoon.ui").nav_prev()
-                elseif args.args == "file1" then
-                    require("harpoon.ui").nav_file(1)
-                elseif args.args == "file2" then
-                    require("harpoon.ui").nav_file(2)
-                elseif args.args == "file3" then
-                    require("harpoon.ui").nav_file(3)
-                elseif args.args == "file4" then
-                    require("harpoon.ui").nav_file(4)
-                end
-            end, {nargs = "*", desc = "Run harpoon"})
-        end
+        config = function(_, opts) end
     }, {
         "RRethy/vim-illuminate",
         event = "BufReadPost",
         opts = {delay = 200, filetypes_denylist = {"NvimTree", "alpha"}},
         config = function(_, opts) require("illuminate").configure(opts) end,
         keys = {
-            {"]]", function() require("illuminate").goto_next_reference(false) end, desc = "Next Reference"},
-            {"[[", function() require("illuminate").goto_prev_reference(false) end, desc = "Prev Reference"}
+            {
+                "]]",
+                function()
+                    require("illuminate").goto_next_reference(false)
+                end,
+                desc = "Next Reference"
+            }, {
+                "[[",
+                function()
+                    require("illuminate").goto_prev_reference(false)
+                end,
+                desc = "Prev Reference"
+            }
         }
-    }, {"luukvbaal/stabilize.nvim", config = function() require("stabilize").setup() end} -- not needed in nvim 0.9
+    }, {
+        "luukvbaal/stabilize.nvim",
+        config = function() require("stabilize").setup() end
+    } -- not needed in nvim 0.9
 }

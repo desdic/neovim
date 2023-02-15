39de0f973029fe52901081fc70d8316546d21e94
Kim Gert Nielsen <kgn@one.com>
2023-02-15 11:32:35 +0100

    Use spaces

--- snippets/python.lua
+++ snippets/python.lua
@@ -35,11 +35,11 @@ local empty_python = function(position)
             t({"", "", ""}), t({[[import argparse]], [[import logging]]}),
             t({"", "", ""}), t("logger = logging.getLogger(__name__)"),
             t({"", ""}), t({"", "", ""}), t({
-                [[def main():]], [[	arg_parser = argparse.ArgumentParser()]],
-                [[	arg_parser.add_argument("--debug", "-d", action="store_true", help="enable debug")]],
-                [[	args = arg_parser.parse_args()]], [[]],
-                [[	logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)]]
-            }), t({"", "", ""}), t({"	"}), i(3), t({"", "", ""}),
+                [[def main():]], [[    arg_parser = argparse.ArgumentParser()]],
+                [[    arg_parser.add_argument("--debug", "-d", action="store_true", help="enable debug")]],
+                [[    args = arg_parser.parse_args()]], [[]],
+                [[    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)]]
+            }), t({"", "", ""}), t({"    "}), i(3), t({"", "", ""}),
             t([[if __name__ == "__main__":]]), t({"", ""}), t([[    main()]])
         })
     end, {})

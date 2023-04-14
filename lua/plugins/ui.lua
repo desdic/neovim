0e1c7d4f41e31c547a4eb0448f74c8362d111054
Kim Gert Nielsen <kgn@one.com>
2023-04-14 07:43:27 +0200

    Add highligts for bufferline to come from catppuccin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -320,13 +320,31 @@ return {
             { "<S-l>", ":BufferLineCycleNext<CR>", desc = "Move to next buffer" },
             { "<S-h>", ":BufferLineCyclePrev<CR>", desc = "Move to previous buffer" },
         },
-        opts = {
-            options = {
-                show_buffer_close_icons = false,
-                show_close_icon = false,
-                persist_buffer_sort = true,
-                offsets = { { filetype = "NvimTree", text = "NvimTree", highlight = "Directory", text_align = "left" } },
-            },
-        },
+        config = function(_, opts)
+            local mocha = require("catppuccin.palettes").get_palette("mocha")
+            require("bufferline").setup({
+                options = {
+                    show_buffer_close_icons = false,
+                    show_close_icon = false,
+                    persist_buffer_sort = true,
+                    offsets = {
+                        { filetype = "NvimTree", text = "NvimTree", highlight = "Directory", text_align = "left" },
+                    },
+                },
+                highlights = require("catppuccin.groups.integrations.bufferline").get({
+                    custom = {
+                        all = {
+                            fill = { bg = "#000000" },
+                        },
+                        mocha = {
+                            background = { fg = mocha.text },
+                        },
+                        latte = {
+                            background = { fg = "#000000" },
+                        },
+                    },
+                }),
+            })
+        end,
     },
 }

52e4a262fddc3202d459d6112668a4c37555ab0f
Kim Gert Nielsen <kn@insecurity.dk>
2023-04-24 08:12:22 +0200

    Remove unused insert mode

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -23,12 +23,6 @@ return {
                         target_windows = { winid },
                         targets = require("utils/lines").get_line_starts(winid),
                     })
-                    vim.schedule(function()
-                        vim.cmd([[norm o]])
-                    end)
-                    vim.schedule(function()
-                        vim.cmd([[startinsert]])
-                    end)
                 end,
             },
         },

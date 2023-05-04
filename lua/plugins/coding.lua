5f326561a237466d746b8e76a2ccf0fac84dd6a6
Kim Gert Nielsen <kn@insecurity.dk>
2023-05-04 08:44:46 +0200

    update dependencies

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -249,8 +249,9 @@ return {
     },
     {
         "ray-x/go.nvim",
-        ft = "go",
-        dependencies = { "ray-x/guihua.lua" },
+        ft = {"go", "gomod"},
+        dependencies = { "ray-x/guihua.lua", "neovim/nvim-lspconfig", "nvim-treesitter/nvim-treesitter" },
+        build = ':lua require("go.install").update_all_sync()',
         opts = { dap_debug = true, dap_debug_gui = true, lsp_inlay_hints = { enable = false } },
         config = function(_, opts)
             require("go").setup(opts)

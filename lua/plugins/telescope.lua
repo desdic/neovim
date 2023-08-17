5bc1aae523c86bdc9fdce1e72dbbe105ad37e29e
Kim Gert Nielsen <kn@insecurity.dk>
2023-08-17 10:42:14 +0200

    Removed commented code

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,3 @@
--- vim.cmd([[
--- function!   QuickFixOpenAll()
---     if empty(getqflist())
---         return
---     endif
---     let s:prev_val = ""
---     for d in getqflist()
---         let s:curr_val = bufname(d.bufnr)
---         if (s:curr_val != s:prev_val)
---             exec "edit " . s:curr_val
---         endif
---         let s:prev_val = s:curr_val
---     endfor
--- endfunction
--- ]])
-
 local M = {
     "nvim-telescope/telescope.nvim",
     -- event = "VeryLazy",

a1e9257a0d2e5c33e42330b83473ab0be1110d86
Kim Gert Nielsen <kn@insecurity.dk>
2023-09-09 12:05:38 +0200

    Move to glow since peek's deno is broken

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -84,23 +84,12 @@ return {
         opts = { input_buffer_type = "dressing" },
     },
     {
-        "toppair/peek.nvim",
-        build = "deno task --quiet build:fast",
-        opts = {},
+        "ellisonleao/glow.nvim",
+        config = true,
+        cmd = "Glow",
         keys = {
-            {
-                "<Leader>op",
-                function()
-                    local peek = require("peek")
-                    if peek.is_open() then
-                        peek.close()
-                    else
-                        peek.open()
-                    end
-                end,
-                desc = "Peek (Markdown Preview)",
-            },
-        },
+            { "<Leader>op", "<cmd>Glow<CR>" }
+        }
     },
     {
         "akinsho/toggleterm.nvim",

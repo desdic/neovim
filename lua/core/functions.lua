4ed4bcbd448562b58255040806f36260a88acb46
Kim Gert Nielsen <kn@insecurity.dk>
2023-09-16 09:28:38 +0200

    Document that MasonUpdate does not update tools only registry

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_user_command("UpdateAll", function()
     vim.cmd([[Lazy sync]])
     vim.cmd([[TSUpdateSync]])
-    vim.cmd([[MasonUpdate]])
+    vim.cmd([[MasonUpdate]]) -- Only updates the registry not the packages
 end, {})

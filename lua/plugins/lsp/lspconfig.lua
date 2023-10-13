36db6a22431dffedba035c0ca82af5c64cf9cd05
Kim Gert Nielsen <kn@insecurity.dk>
2023-10-13 07:37:56 +0200

    Setup keymaps for lsp in lspconfig

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -3,17 +3,121 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
+        "stevearc/aerial.nvim",
     },
     config = function()
         local lspconfig = require("lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
+        local capabilities = cmp_nvim_lsp.default_capabilities()
 
         local on_attach = function(client, bufnr)
-            require("core.format").on_attach(client, bufnr)
-            require("core.keymaps_lsp").on_attach(client, bufnr)
-        end
+            -- Check for capabilities
+            local has_cap = function(cap)
+                return client.server_capabilities[cap .. "Provider"]
+            end
 
-        local capabilities = cmp_nvim_lsp.default_capabilities()
+            local has_plugin = function(plugin)
+                return pcall(require, plugin)
+            end
+
+            require("core.format").on_attach(client, bufnr)
+
+            local keymap = vim.keymap
+            local format = require("core.format").format
+
+            keymap.set(
+                "n",
+                "gl",
+                vim.diagnostic.open_float,
+                { silent = true, buffer = bufnr, desc = "Line diagnostics" }
+            )
+            keymap.set("n", "<leader>cl", "<cmd>LspInfo<cr>", { silent = true, desc = "LSP info" })
+            keymap.set("n", "<leader>cr", "<cmd>LspRestart<cr>", { silent = true, desc = "Restart LSP server" })
+            keymap.set(
+                "n",
+                "<leader>xd",
+                "<cmd>Telescope diagnostics<cr>",
+                { silent = true, desc = "Telescope Diagnostics" }
+            )
+            keymap.set(
+                "n",
+                "gd",
+                "<cmd>Telescope lsp_definitions<cr>",
+                { silent = true, buffer = bufnr, desc = "Goto Definition" }
+            )
+            keymap.set(
+                "n",
+                "gr",
+                "<cmd>Telescope lsp_references<cr>",
+                { silent = true, buffer = bufnr, desc = "References" }
+            )
+            keymap.set(
+                "n",
+                "gi",
+                "<cmd>Telescope lsp_implementations<cr>",
+                { silent = true, buffer = bufnr, desc = "Goto Implementation" }
+            )
+            keymap.set(
+                "n",
+                "gt",
+                "<cmd>Telescope lsp_type_definitions<cr>",
+                { silent = true, buffer = bufnr, desc = "Goto Type Definition" }
+            )
+            keymap.set("n", "K", vim.lsp.buf.hover, { silent = true, buffer = bufnr, desc = "Hover" })
+
+            keymap.set(
+                "n",
+                "<C-j>",
+                vim.diagnostic.goto_next,
+                { silent = true, buffer = bufnr, desc = "Next Diagnostic" }
+            )
+            keymap.set(
+                "n",
+                "<C-k>",
+                vim.diagnostic.goto_prev,
+                { silent = true, buffer = bufnr, desc = "Prev Diagnostic" }
+            )
+
+
+            if has_cap("signatureHelp") then
+                keymap.set(
+                    "n",
+                    "gs",
+                    vim.lsp.buf.signature_help,
+                    { silent = true, buffer = bufnr, desc = "Signature Help" }
+                )
+            end
+
+            if has_plugin("aerial") then
+                keymap.set("n", "{", "<cmd>AerialNext<cr>", { silent = true, buffer = bufnr, desc = "Aerial next" })
+                keymap.set("n", "}", "<cmd>AerialPrev<cr>", { silent = true, buffer = bufnr, desc = "Aerial prev" })
+            end
+
+            if not has_plugin("inc_rename") then
+                if has_cap("rename") then
+                    keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+                end
+            end
+
+            if has_cap("codeAction") then
+                keymap.set({ "n", "v" }, "<leader>ca", function()
+                    if vim.bo.filetype == "go" then
+                        if has_plugin("go") then
+                            return vim.cmd("GoCodeAction")
+                        end
+                    end
+                    return vim.lsp.buf.code_action()
+                end, { silent = true, buffer = bufnr, desc = "Code Action" })
+            end
+
+            if has_cap("documentFormatting") then
+                keymap.set("n", "<leader>f", format, { silent = true, buffer = bufnr, desc = "Format Document" })
+            end
+
+            if has_cap("documentRangeFormatting") then
+                keymap.set("v", "<leader>f", format, { silent = true, buffer = bufnr, desc = "Format Range" })
+            end
+        end
 
         -- {{{ Lua
         lspconfig["lua_ls"].setup({

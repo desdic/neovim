ca6b0c91e8078c27f29af0377d5191a651d32ca8
Kim Gert Nielsen <kn@insecurity.dk>
2023-11-26 10:31:59 +0100

    More flash features enabled

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -7,12 +7,9 @@ return {
         },
         modes = {
             search = {
-                enabled = false     -- disable for search
+                enabled = false, -- disable for search
             },
-            char = {
-                enabled = false     -- disable for fFtT
-            }
-        }
+        },
     },
     keys = {
         {
@@ -21,12 +18,59 @@ return {
             function()
                 require("flash").jump()
             end,
+            desc = "Flash",
         },
         {
             "S",
-            mode = { "o", "x" },
+            mode = { "n", "x", "o" },
             function()
                 require("flash").treesitter()
+            end,
+            desc = "Flash Treesitter",
+        },
+        {
+            "r",
+            mode = "o",
+            function()
+                require("flash").remote()
+            end,
+            desc = "Remote Flash",
+        },
+        {
+            "R",
+            mode = { "o", "x" },
+            function()
+                require("flash").treesitter_search()
+            end,
+            desc = "Treesitter Search",
+        },
+        {
+            "<c-s>",
+            mode = { "c" },
+            function()
+                require("flash").toggle()
+            end,
+            desc = "Toggle Flash Search",
+        },
+        {
+            "<c-s>",
+            mode = { "n" },
+            function()
+                require("flash").jump({
+                    pattern = ".", -- initialize pattern with any char
+                    search = {
+                        mode = function(pattern)
+                            -- remove leading dot
+                            if pattern:sub(1, 1) == "." then
+                                pattern = pattern:sub(2)
+                            end
+                            -- return word pattern and proper skip pattern
+                            return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+                        end,
+                    },
+                    -- select the range
+                    jump = { pos = "range" },
+                })
             end,
         },
     },

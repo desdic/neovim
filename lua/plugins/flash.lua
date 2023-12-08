a0aa19804becfd9abe66cc218e086d1da608ec10
Kim Gert Nielsen <kn@insecurity.dk>
2023-12-08 12:48:15 +0100

    Remove redundant and unused keymappings

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -51,35 +51,6 @@ return {
                 require("flash").treesitter_search()
             end,
             desc = "Treesitter Search",
-        },
-        {
-            "<c-s>",
-            mode = { "c" },
-            function()
-                require("flash").toggle()
-            end,
-            desc = "Toggle Flash Search",
-        },
-        {
-            "<c-s>",
-            mode = { "n" },
-            function()
-                require("flash").jump({
-                    pattern = ".", -- initialize pattern with any char
-                    search = {
-                        mode = function(pattern)
-                            -- remove leading dot
-                            if pattern:sub(1, 1) == "." then
-                                pattern = pattern:sub(2)
-                            end
-                            -- return word pattern and proper skip pattern
-                            return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
-                        end,
-                    },
-                    -- select the range
-                    jump = { pos = "range" },
-                })
-            end,
-        },
+        }
     },
 }

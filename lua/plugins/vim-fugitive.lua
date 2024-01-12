dce1801a593c39521ff449fd27e0048269271f65
Kim Gert Nielsen <kn@insecurity.dk>
2024-01-12 07:21:35 +0100

    Add descriptions

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -16,14 +16,14 @@ return {
                 end
 
                 local bufnr = vim.api.nvim_get_current_buf()
-                local opts = { buffer = bufnr, remap = false }
+                local opts = { buffer = bufnr, remap = false, desc="Git push" }
                 vim.keymap.set("n", "<leader>p", function()
                     vim.cmd.Git("push")
                 end, opts)
 
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                 -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", { buffer = bufnr, remap = false, desc="Git push" })
+                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", { buffer = bufnr, remap = false, desc="Git push origin" })
                 vim.keymap.set("n", "gh", "<cmd>diffget //2<cr>", { buffer = bufnr, remap = false, desc="Git choose left" })
                 vim.keymap.set("n", "gl", "<cmd>diffget //3<cr>", { buffer = bufnr, remap = false, desc="Git choose right" })
             end,

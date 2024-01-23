dead2b750513be32447789298d8cbc516ac66b3e
Kim Gert Nielsen <kn@insecurity.dk>
2024-01-23 14:28:59 +0100

    Remove keys not used

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -2,27 +2,4 @@ return {
     "folke/persistence.nvim",
     event = "BufReadPre",
     opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
-    keys = {
-        {
-            "<leader>qs",
-            function()
-                require("persistence").load()
-            end,
-            desc = "Restore Session",
-        },
-        {
-            "<leader>ql",
-            function()
-                require("persistence").load()
-            end,
-            desc = "Restore Last Session",
-        },
-        {
-            "<leader>qd",
-            function()
-                require("persistence").stop()
-            end,
-            desc = "Don't Save Current Session",
-        },
-    },
 }

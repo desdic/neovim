d1832d30b2047783efc9b6f20e29bb019c108232
Kim Gert Nielsen <kn@insecurity.dk>
2024-01-23 14:27:29 +0100

    Move all mini based plugins into mini.lua

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -43,5 +43,31 @@ return {
         config = function(_, opts)
             require("mini.move").setup(opts)
         end,
-    }
+    },
+    {
+        "echasnovski/mini.surround",
+        opts = {
+            mappings = {
+                add = "gsa", -- Add surrounding in Normal and Visual modes
+                delete = "gsd", -- Delete surrounding
+                find = "gsf", -- Find surrounding (to the right)
+                find_left = "gsF", -- Find surrounding (to the left)
+                highlight = "gsh", -- Highlight surrounding
+                replace = "gsr", -- Replace surrounding
+                update_n_lines = "gsn", -- Update `n_lines`
+            },
+        },
+    },
+    {
+        "echasnovski/mini.comment",
+        version = "*",
+        opts = {
+            options = {
+                custom_commentstring = function()
+                    return require("ts_context_commentstring.internal").calculate_commentstring()
+                        or vim.bo.commentstring
+                end,
+            },
+        },
+    },
 }

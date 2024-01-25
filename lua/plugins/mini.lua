1a6ded9e5bceac768851e6629bba98c49ce17802
Kim Gert Nielsen <kn@insecurity.dk>
2024-01-25 18:25:19 +0100

    Cleanup

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -18,31 +18,6 @@ return {
                 desc = "[B]uffer [d]elete force",
             },
         },
-        config = function()
-            require("mini.bufremove").setup()
-        end,
-    },
-    {
-        "echasnovski/mini.move",
-        event = "BufEnter",
-        opts = {
-            mappings = {
-                -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-                left = "<M-S-h>",
-                right = "<M-S-l>",
-                down = "<M-S-j>",
-                up = "<M-S-k>",
-
-                -- Move current line in Normal mode
-                line_left = "<M-S-h>",
-                line_right = "<M-S-l>",
-                line_down = "<M-S-j>",
-                line_up = "<M-S-k>",
-            },
-        },
-        config = function(_, opts)
-            require("mini.move").setup(opts)
-        end,
     },
     {
         "echasnovski/mini.surround",

14e8b2359b94dd21463235c24863416990c984e3
Kim Gert Nielsen <kn@insecurity.dk>
2024-01-28 17:57:39 +0100

    fix: Better formatting

--- lua/plugins/inc-rename.lua
+++ lua/plugins/inc-rename.lua
@@ -2,6 +2,15 @@ return {
     "smjonas/inc-rename.nvim",
     dependencies = { "stevearc/dressing.nvim" },
     cmd = "IncRename",
-    keys = { { "<Leader>rn", function() return ":IncRename " .. vim.fn.expand("<cword>") end, expr = true, desc = "[R]e[n]ame" } },
+    keys = {
+        {
+            "<Leader>rn",
+            function()
+                return ":IncRename " .. vim.fn.expand("<cword>")
+            end,
+            expr = true,
+            desc = "[R]e[n]ame",
+        },
+    },
     opts = { input_buffer_type = "dressing" },
 }

c1613bb3ae184df28248e36f320c6647106ea5ce
Kim Gert Nielsen <kn@insecurity.dk>
2024-01-28 17:44:06 +0100

    fix: Fix description

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,4 +1,4 @@
 local bufnr = vim.api.nvim_get_current_buf()
 vim.keymap.set("n", "<leader>ca", function()
     vim.cmd.RustLsp("codeAction")
-end, { silent = true, buffer = bufnr })
+end, { silent = true, buffer = bufnr, desc = "RustCodeAction" })

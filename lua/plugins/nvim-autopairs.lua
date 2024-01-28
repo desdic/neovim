34eb98ced29913efe0093329181c3f4ce53872f0
Kim Gert Nielsen <kn@insecurity.dk>
2024-01-28 13:08:36 +0100

    fix: remove autopairs plugin

--- lua/plugins/nvim-autopairs.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "windwp/nvim-autopairs",
-    event = "VeryLazy",
-    opts = {
-        check_ts = true,
-        disable_filetype = { "TelescopePrompt", "vim" },
-        fast_wrap = {},
-    },
-}

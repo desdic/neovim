b4e1e1db2a21b536acd32e494adbff589be82b64
Kim Gert Nielsen <kn@insecurity.dk>
2024-01-28 16:58:09 +0100

    fix: remove mini buf in favour of build-in

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,25 +1,5 @@
 return {
     {
-        "echasnovski/mini.bufremove",
-        event = "VeryLazy",
-        keys = {
-            {
-                "<leader>bd",
-                function()
-                    require("mini.bufremove").delete(0, false)
-                end,
-                desc = "[B]uffer [d]elete",
-            },
-            {
-                "<leader>bD",
-                function()
-                    require("mini.bufremove").delete(0, true)
-                end,
-                desc = "[B]uffer [d]elete force",
-            },
-        },
-    },
-    {
         "echasnovski/mini.comment",
         version = "*",
         opts = {

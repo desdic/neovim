658237372a5e998fa17ad1bc4e50d186453d5575
Kim Gert Nielsen <kn@insecurity.dk>
2024-02-26 16:42:11 +0100

    Use and test marlin

--- lua/plugins/marlin.lua
+++ lua/plugins/marlin.lua
@@ -14,6 +14,7 @@ return {
         end, { desc = "add file" })
         keymap("n", "<Leader>fd", function()
             marlin.remove()
+            vim.cmd("bdelete")
         end, { desc = "remove file" })
         keymap("n", "<Leader>f]", function()
             marlin.move_up()
@@ -27,6 +28,44 @@ return {
         keymap("n", "<Leader>0", function()
             marlin.open_all()
         end, { desc = "open all" })
+
+        keymap("n", "<Leader>fx", function()
+            local conf = require("telescope.config").values
+            local action_state = require("telescope.actions.state")
+            local results = require("marlin").get_indexes()
+
+            local index = 0
+            require("telescope.pickers")
+                .new({}, {
+                    prompt_title = "Marlin",
+                    finder = require("telescope.finders").new_table({
+                        results = results,
+                        entry_maker = function(entry)
+                            index = index + 1
+                            return {
+                                value = entry,
+                                ordinal = index .. ":" .. entry.filename,
+                                lnum = entry.row,
+                                col = entry.col + 1,
+                                filename = entry.filename,
+                                display = index .. ":" .. entry.filename .. ":" .. entry.row .. ":" .. entry.col,
+                            }
+                        end,
+                    }),
+                    previewer = conf.grep_previewer({}),
+                    sorter = conf.generic_sorter({}),
+                    attach_mappings = function(_, map)
+                        map("i", "<c-d>", function(bufnr)
+                            local current_picker = action_state.get_current_picker(bufnr)
+                            current_picker:delete_selection(function(selection)
+                                require("marlin").remove(selection.filename)
+                            end)
+                        end)
+                        return true
+                    end,
+                })
+                :find()
+        end, { desc = "Telescope marlin" })
 
         for index = 1, 4 do
             keymap("n", "<Leader>" .. index, function()

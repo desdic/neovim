73c575e8e5323d365d7d5e8d7dc46c852c5b7d9f
Kim Gert Nielsen <kn@insecurity.dk>
2024-03-10 17:39:07 +0100

    Remove virtual text toggle and remove virtual next from xray-go

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -3,7 +3,12 @@ return {
     ft = { "go", "gomod" },
     dependencies = { "ray-x/guihua.lua", "neovim/nvim-lspconfig", "nvim-treesitter/nvim-treesitter" },
     build = ':lua require("go.install").update_all_sync()',
-    opts = { dap_debug = true, dap_debug_gui = true, lsp_inlay_hints = { enable = false } },
+    opts = {
+        dap_debug = true,
+        dap_debug_gui = true,
+        lsp_inlay_hints = { enable = false },
+        diagnostic = { virtual_text = false },
+    },
     config = function(_, opts)
         require("go").setup(opts)
     end,

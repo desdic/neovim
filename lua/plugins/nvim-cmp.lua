1d17e095c5358f03452096d92faf4611187e5b61
Kim Gert Nielsen <kn@insecurity.dk>
2024-04-01 09:10:37 +0200

    Add cmp to command line

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -93,5 +93,17 @@ return {
             },
             window = { documentation = cmp.config.window.bordered(), completion = cmp.config.window.bordered() },
         })
+
+        -- `:` cmdline setup.
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = "path" },
+            }, {
+                { name = "cmdline" },
+            }),
+            matching = { disallow_symbol_nonprefix_matching = false },
+        })
+
     end,
 }

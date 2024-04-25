bf86d5ab26e0738634fd36988bd0d37a22186b78
Kim Gert Nielsen <kn@insecurity.dk>
2024-04-25 07:27:20 +0200

    Prepare it for 0.10

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -131,11 +131,13 @@ function M.config()
     local ts = require("telescope")
 
     local actions = require("telescope.actions")
+    local empty = vim.empty or vim.tbl_isempty
 
+    -- Open multiple files
     local select_one_or_multi = function(prompt_bufnr)
         local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
         local multi = picker:get_multi_selection()
-        if not vim.tbl_isempty(multi) then
+        if not empty(multi) then
             require("telescope.actions").close(prompt_bufnr)
             for _, j in pairs(multi) do
                 if j.path ~= nil then

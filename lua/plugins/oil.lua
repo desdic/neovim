9509f0bf9747ac62cf48a5c3a10f930074a0db6f
Kim Gert Nielsen <kn@insecurity.dk>
2024-08-02 09:12:10 +0200

    Wrap oil around my fzf cache

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -11,7 +11,28 @@ return {
             "<cmd>Oil<CR>",
             desc = "Browse files/dirs",
         },
+        {
+            "<leader>re",
+            function()
+                local filename = vim.fn.expand("$HOME/tmp/sshfzf.cache")
+                local file = io.open(filename, "rb")
+                if file then
+                    local lines = {}
+                    for line in io.lines(filename) do
+                        table.insert(lines, line)
+                    end
+                    file:close()
+                    vim.ui.select(lines, { prompt = "Choose remote host" }, function(choice)
+                        if choice then
+                            vim.cmd("Oil oil-ssh://" .. choice .. "/")
+                        end
+                    end)
+                end
+            end,
+            desc = "Browse remote files/dirs",
+        },
     },
+    cmd = { "Oil" },
     -- Optional dependencies
     -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons

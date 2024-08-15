068e5746f352f2699c02320cb2fbbb496b666d2f
Kim Gert Nielsen <kim.nielsen@group.one>
2024-08-15 09:54:54 +0200

    Only lint when writing or reading

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -40,7 +40,7 @@ return {
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
             group = lint_augroup,
             callback = function()
                 lint.try_lint()

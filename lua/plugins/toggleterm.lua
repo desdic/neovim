7e137065d518cfb6e761f4a4f8f32ee973c7e5f9
Kim Gert Nielsen <kim.nielsen@group.one>
2024-08-30 08:53:24 +0200

    fix: add float again

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,7 +6,25 @@ return {
         { "<leader>tts", "<cmd>TermSelect<cr>", desc = "Toggle term select" },
     },
     version = "*",
-    opts = {},
+    opts = {
+        size = 20,
+        open_mapping = [[<c-t>]],
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persist_size = true,
+        direction = "float",
+        close_on_exit = true,
+        shell = vim.o.shell,
+        float_opts = {
+            border = "curved",
+            winblend = 0,
+            highlights = { border = "Normal", background = "Normal" },
+        },
+    },
     config = function(_, opts)
         require("toggleterm").setup(opts)
 

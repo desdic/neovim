0e6e7e327c8b6190196ba79cd2ed461f188068b3
Kim Gert Nielsen <kn@insecurity.dk>
2024-09-22 12:05:42 +0200

    feature: hook quickfix to makefile extension

--- lua/plugins/greyjoy.lua
+++ lua/plugins/greyjoy.lua
@@ -2,8 +2,7 @@ return {
     "desdic/greyjoy.nvim",
     keys = {
         { "<Leader>gr", "<cmd>GreyjoyTelescope<CR>", desc = "[G]reyjoy [r]un" },
-        -- { "<Leader>gr", "<cmd>Greyjoy<CR>", desc = "[G]reyjoy [r]un" },
-        { "<Leader>gg", "<cmd>Greyjoy fast<CR>", desc = "[G]reyjoy fast [g]roup" },
+        { "<Leader>gg", "<cmd>GreyjoyTelescope fast<CR>", desc = "[G]reyjoy fast [g]roup" },
         { "<Leader>ge", "<cmd>Greyedit<CR>", desc = "[G]reyjoy [r]un" },
     },
     dependencies = {
@@ -13,6 +12,37 @@ return {
     config = function()
         local greyjoy = require("greyjoy")
         local condition = require("greyjoy.conditions")
+
+        local tmpmakename = nil
+
+        local pre_make = function(command)
+            tmpmakename = os.tmpname()
+            table.insert(command.command, "2>&1")
+            table.insert(command.command, "|")
+            table.insert(command.command, "tee")
+            table.insert(command.command, tmpmakename)
+        end
+
+        -- A bit hacky solution to checking when tee has flushed its file
+        local post_make = function()
+            vim.cmd(":cexpr []")
+            local cmd = { "inotifywait", "-e", "close_write", tmpmakename }
+
+            local job_id = vim.fn.jobstart(cmd, {
+                stdout_buffered = true,
+                on_exit = function(_, _, _)
+                    if tmpmakename ~= nil then
+                        vim.cmd(":cgetfile " .. tmpmakename)
+                        os.remove(tmpmakename)
+                    end
+                end,
+            })
+
+            if job_id <= 0 then
+                vim.notify("Failed to start inotifywait!")
+            end
+        end
+
         greyjoy.setup({
             output_results = "toggleterm",
             last_first = true,
@@ -63,6 +93,10 @@ return {
                 },
                 docker_compose = { group_id = 3 },
                 cargo = { group_id = 4 },
+                makefile = {
+                    pre_hook = pre_make,
+                    post_hook = post_make,
+                },
             },
             run_groups = { fast = { "generic", "makefile", "cargo", "docker_compose" } },
         })

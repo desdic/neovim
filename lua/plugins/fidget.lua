f71da40667dfecdb64b15d33d39bf7495c3e051d
Kim Gert Nielsen <kn@insecurity.dk>
2024-09-29 11:27:22 +0200

    use fidget for notify

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,16 +1,14 @@
 return {
     "j-hui/fidget.nvim",
-    -- event = { "BufReadPre", "BufNewFile" },
     lazy = false,
     priority = 1001,
     opts = {
         notification = {
-            filter = vim.log.levels.DEBUG,
+            override_vim_notify = true,
         },
     },
     config = function(_, opts)
         local fidget = require("fidget")
         fidget.setup(opts)
-        vim.notify = fidget.notify
     end,
 }

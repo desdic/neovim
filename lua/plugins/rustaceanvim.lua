a53532d603f856343035f533cafe712a4004b4df
Kim Gert Nielsen <kn@insecurity.dk>
2024-10-07 06:37:52 +0200

    Only load when needed

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,5 +1,6 @@
 return {
     "mrcjkb/rustaceanvim",
     version = "^5", -- Recommended
-    lazy = false, -- This plugin is already lazy
+    -- lazy = false, -- This plugin is already lazy
+    ft = { "rust" },
 }

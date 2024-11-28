5183906b595c4347fa9e904e8b1fef759ce0e612
Kim Gert Nielsen <kn@insecurity.dk>
2024-11-28 11:17:12 +0100

    don't use cmp in gitcommits

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -50,6 +50,9 @@ return {
             -- will be removed in a future release, assuming themes add support
             use_nvim_cmp_as_default = false,
         },
+        -- don't use in gitcommits
+        blocked_filetypes = { "gitcommit" },
+
         -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- adjusts spacing to ensure icons are aligned
         nerd_font_variant = "mono",

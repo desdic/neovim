f04184a06ea22f6a653c1b3033d1624580b61653
Kim Gert Nielsen <kn@insecurity.dk>
2024-12-18 06:33:26 +0100

    Remove plugins no longer needed

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -68,10 +68,12 @@ return {
         },
         dependencies = {
             {
-                "mfussenegger/nvim-dap-python",
-                ft = { "python" },
+                "rcarriga/nvim-dap-ui",
+                dependencies = {
+                    "nvim-neotest/nvim-nio",
+                },
                 config = function()
-                    require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+                    require("dapui").setup()
                 end,
             },
             {
@@ -82,56 +84,9 @@ return {
                 end,
             },
             {
-                "rcarriga/nvim-dap-ui",
-                config = function()
-                    require("dapui").setup()
-                end,
-            },
-            {
-                "nvim-neotest/nvim-nio",
-            },
-            {
                 "julianolf/nvim-dap-lldb",
                 ft = { "c", "cpp", "rust", "zig" },
                 opts = { codelldb_path = "/sbin/codelldb" },
-            },
-            {
-                "jbyuki/one-small-step-for-vimkind",
-                ft = { "lua" },
-                config = function()
-                    local dap = require("dap")
-                    dap.adapters.nlua = function(callback, conf)
-                        local adapter = {
-                            type = "server",
-                            host = conf.host or "127.0.0.1",
-                            port = conf.port or 8086,
-                        }
-                        if conf.start_neovim then
-                            local dap_run = dap.run
-                            dap.run = function(c)
-                                adapter.port = c.port
-                                adapter.host = c.host
-                            end
-                            require("osv").run_this()
-                            dap.run = dap_run
-                        end
-                        callback(adapter)
-                    end
-                    dap.configurations.lua = {
-                        {
-                            type = "nlua",
-                            request = "attach",
-                            name = "Run this file",
-                            start_neovim = {},
-                        },
-                        {
-                            type = "nlua",
-                            request = "attach",
-                            name = "Attach to running Neovim instance (port = 8086)",
-                            port = 8086,
-                        },
-                    }
-                end,
             },
         },
         config = function()

5225e0e86d6fcd6fb6fe0eaa1de32c0fe9460f54
Kim Gert Nielsen <kn@insecurity.dk>
2025-01-11 11:50:19 +0100

    Remove schemastore

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -53,60 +53,6 @@ return {
                     })
                 end,
 
-                -- Extra settings for LSP
-
-                -- {{ LUA
-                -- ["lua_ls"] = function()
-                --     lspconfig.lua_ls.setup({
-                --
-                --         on_init = function(client)
-                --             if client.workspace_folders then
-                --                 local path = client.workspace_folders[1].name
-                --                 if
-                --                     vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc")
-                --                 then
-                --                     return
-                --                 end
-                --             end
-                --
-                --             client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                --                 runtime = {
-                --                     -- Tell the language server which version of Lua you're using
-                --                     -- (most likely LuaJIT in the case of Neovim)
-                --                     version = "LuaJIT",
-                --                 },
-                --                 -- Make the server aware of Neovim runtime files
-                --                 workspace = {
-                --                     checkThirdParty = false,
-                --                     library = {
-                --                         vim.env.VIMRUNTIME,
-                --                     },
-                --                 },
-                --             })
-                --         end,
-                --         filetypes = { "lua" },
-                --         settings = { -- custom settings for lua
-                --             Lua = {
-                --                 -- make the language server recognize "vim" global
-                --                 diagnostics = { globals = { "vim", "require" } },
-                --                 completion = {
-                --                     callSnippet = "Replace",
-                --                 },
-                --                 workspace = {
-                --                     -- make language server aware of runtime files
-                --                     library = {
-                --                         [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                --                         [vim.fn.stdpath("config") .. "/lua"] = true,
-                --                     },
-                --                 },
-                --                 hint = {
-                --                     enable = true,
-                --                 },
-                --             },
-                --         },
-                --     })
-                -- end,
-
                 -- {{ Go
                 ["gopls"] = function()
                     lspconfig["gopls"].setup({
@@ -164,35 +110,12 @@ return {
 
                 -- {{ JSON
                 ["jsonls"] = function()
-                    lspconfig["jsonls"].setup({
-                        settings = {
-                            settings = {
-                                json = {
-                                    schemas = require("schemastore").json.schemas(),
-                                    validate = { enable = true },
-                                },
-                            },
-                        },
-                    })
+                    lspconfig["jsonls"].setup({})
                 end,
 
                 -- {{ YAML
                 ["yamlls"] = function()
-                    lspconfig["yamlls"].setup({
-                        settings = {
-                            yaml = {
-                                keyOrdering = false,
-                                schemaStore = {
-                                    -- You must disable built-in schemaStore support if you want to use
-                                    -- this plugin and its advanced options like `ignore`.
-                                    enable = false,
-                                    -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-                                    url = "",
-                                },
-                                schemas = require("schemastore").yaml.schemas(),
-                            },
-                        },
-                    })
+                    lspconfig["yamlls"].setup({})
                 end,
 
                 -- {{ Ruby
--- lua/plugins/schemastore.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "b0o/schemastore.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    version = false, -- last release is way too old
-}

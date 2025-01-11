72187a91ca99b0febead50a4dbbdf0e7f99bb19c
Kim Gert Nielsen <kn@insecurity.dk>
2025-01-11 11:30:35 +0100

    Moved all snippets to luasnippets

---
+++ luasnippets/python.lua
@@ -0,0 +1,50 @@
+local ls = require("luasnip")
+local s = ls.s
+local i = ls.i
+local t = ls.t
+local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local sn = ls.snippet_node
+local r = require("luasnip.extras").rep
+local fmt = require("luasnip.extras.fmt").fmt
+
+local snippets, autosnippets = {}, {}
+
+local main = s(
+    { trig = "main", name = "main" },
+    fmt(
+        [[
+#!/usr/bin/env {}
+
+import argparse
+import logging
+
+logger = logging.getLogger(__name__)
+
+def main() -> None:
+{}arg_parser = argparse.ArgumentParser()
+{}arg_parser.add_argument("--debug", "-d", action="store_true", help="enable debug")
+{}args = arg_parser.parse_args()
+
+{}logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+{}{}
+
+if __name__ == "__main__":
+{}main()
+        ]],
+        {
+            c(1, { t("python3"), t("python") }),
+            t({ "\t" }),
+            t({ "\t" }),
+            t({ "\t" }),
+            t({ "\t" }),
+            t({ "\t" }),
+            i(0),
+            t({ "\t" }),
+        }
+    )
+)
+
+table.insert(snippets, main)
+
+return snippets, autosnippets

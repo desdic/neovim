56986e0edaa5222d49d2d71c82302f29a9f863b3
Kim Gert Nielsen <kn@insecurity.dk>
2025-01-20 06:33:51 +0100

    refactor: prefer mini-ai over treesitter

---
+++ lua/plugins/mini-ai.lua
@@ -0,0 +1,5 @@
+return {
+    "echasnovski/mini.ai",
+    event = "VeryLazy",
+    opts = {},
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         build = ":TSUpdate",
         cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
         dependencies = {
-            { "nvim-treesitter/nvim-treesitter-textobjects" },
+            -- { "nvim-treesitter/nvim-treesitter-textobjects" },
             { "JoosepAlviste/nvim-ts-context-commentstring" },
         },
         config = function()
@@ -61,45 +61,45 @@ return {
                         node_decremental = "V",
                     },
                 },
-                textobjects = {
-                    select = {
-                        enable = true,
-
-                        -- Automatically jump forward to textobj, similar to targets.vim
-                        lookahead = true,
-
-                        keymaps = {
-                            ["aa"] = "@parameter.outer",
-                            ["ia"] = "@parameter.inner",
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true, -- whether to set jumps in the jumplist
-                        goto_next_start = {
-                            ["]f"] = "@function.outer",
-                            ["]b"] = "@block.inner",
-                            ["]c"] = "@class.outer",
-                            ["]l"] = "@loop.outer",
-                            ["]i"] = "@conditional.outer",
-                            ["]p"] = "@parameter.inner",
-                            ["]o"] = "@call.outer",
-                        },
-                        goto_previous_start = {
-                            ["[f"] = "@function.outer",
-                            ["[b"] = "@block.inner",
-                            ["[c"] = "@class.outer",
-                            ["[l"] = "@loop.outer",
-                            ["[i"] = "@conditional.outer",
-                            ["[p"] = "@parameter.inner",
-                            ["[o"] = "@call.outer",
-                        },
-                    },
-                },
+                -- textobjects = {
+                --     select = {
+                --         enable = false,
+                --
+                --         -- Automatically jump forward to textobj, similar to targets.vim
+                --         lookahead = true,
+                --
+                --         keymaps = {
+                --             ["aa"] = "@parameter.outer",
+                --             ["ia"] = "@parameter.inner",
+                --             ["af"] = "@function.outer",
+                --             ["if"] = "@function.inner",
+                --             ["ac"] = "@class.outer",
+                --             ["ic"] = "@class.inner",
+                --         },
+                --     },
+                --     move = {
+                --         enable = true,
+                --         set_jumps = true, -- whether to set jumps in the jumplist
+                --         goto_next_start = {
+                --             ["]f"] = "@function.outer",
+                --             ["]b"] = "@block.inner",
+                --             ["]c"] = "@class.outer",
+                --             ["]l"] = "@loop.outer",
+                --             ["]i"] = "@conditional.outer",
+                --             ["]p"] = "@parameter.inner",
+                --             ["]o"] = "@call.outer",
+                --         },
+                --         goto_previous_start = {
+                --             ["[f"] = "@function.outer",
+                --             ["[b"] = "@block.inner",
+                --             ["[c"] = "@class.outer",
+                --             ["[l"] = "@loop.outer",
+                --             ["[i"] = "@conditional.outer",
+                --             ["[p"] = "@parameter.inner",
+                --             ["[o"] = "@call.outer",
+                --         },
+                --     },
+                -- },
             })
         end,
     },

dd2bac2c577ec262098e6be5d509ee7c852fb472
Kim Gert Nielsen <kn@insecurity.dk>
2025-03-27 21:43:05 +0100

    Mason is no longer use so function is not in use any more

--- lua/core/functions.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_create_user_command("UpdateAll", function()
-    vim.cmd([[Lazy sync]])
-    vim.cmd([[TSUpdateSync]])
-    vim.cmd([[MasonUpdate]]) -- Only updates the registry not the packages
-end, {})

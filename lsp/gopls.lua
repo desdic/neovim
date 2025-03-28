88347e663be54d9da3fa366afe90a713962d475e
Kim Gert Nielsen <kn@insecurity.dk>
2025-03-28 13:33:26 +0100

    Add options to gopls

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -3,4 +3,16 @@ return {
     filetypes = { "go", "gomod", "gowork", "gotmpl" },
     root_markers = { "go.work", "go.mod", ".git" },
     single_file_support = true,
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+                unusedwrite = true,
+                nilness = true,
+            },
+            gofumpt = true,
+            semanticTokens = true,
+            staticcheck = true,
+        },
+    },
 }

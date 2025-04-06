2c254970cfe21a3c1a85098001207a1d24683dbf
Kim Gert Nielsen <kn@insecurity.dk>
2025-04-06 12:15:20 +0200

    make sure last_run remembers binary

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -86,7 +86,50 @@ return {
             {
                 "julianolf/nvim-dap-lldb",
                 ft = { "c", "cpp", "rust", "zig" },
-                opts = { codelldb_path = "/sbin/codelldb" },
+                config = function()
+                    local dap = require("dap")
+                    local last_program = nil
+
+                    dap.adapters.lldb = {
+                        type = "executable",
+                        command = "/sbin/codelldb",
+                        name = "lldb",
+                    }
+
+                    dap.configurations.cpp = {
+                        {
+                            name = "Debug with LLDB",
+                            type = "lldb",
+                            request = "launch",
+                            program = function()
+                                if last_program then
+                                    return last_program
+                                end
+                                last_program = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                                return last_program
+                            end,
+                            cwd = "${workspaceFolder}",
+                            stopOnEntry = false,
+                            args = {},
+                            runInTerminal = false,
+                        },
+                    }
+
+                    vim.api.nvim_create_user_command("DapResetBinary", function()
+                        last_program = nil
+                        print("DAP binary reset. Next debug session will ask for a path.")
+                    end, {})
+
+                    dap.configurations.c = dap.configurations.cpp
+                    dap.configurations.rust = dap.configurations.cpp
+                    dap.configurations.zig = dap.configurations.cpp
+                end,
+                -- opts = {
+                --     codelldb_path = "/sbin/codelldb",
+                --     program = function()
+                --         return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                --     end,
+                -- },
             },
             {
                 "jbyuki/one-small-step-for-vimkind",

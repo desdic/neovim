e5ffc772a29f41c5393fbff32e92002f8a44bfb6
Kim Gert Nielsen <kn@insecurity.dk>
2025-05-08 11:18:08 +0200

    remove Tab

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -19,8 +19,7 @@ return {
         keymap = {
             ["<CR>"] = { "accept", "fallback" },
             ["<C-\\>"] = { "hide", "fallback" },
-            ["<C-n>"] = { "select_next", "show" },
-            ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+            ["<C-n>"] = { "select_next", "snippet_forward", "fallback" },
             ["<C-p>"] = { "select_prev" },
             ["<C-b>"] = { "scroll_documentation_up", "fallback" },
             ["<C-f>"] = { "scroll_documentation_down", "fallback" },

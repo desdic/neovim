b18ed4c209033741d4789ca25cdf84df7b9159d0
Kim Gert Nielsen <kn@insecurity.dk>
2025-05-08 11:18:13 +0200

    Remove gci

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,7 +8,7 @@ return {
             formatters_by_ft = {
                 c = { name = "clangd", timeout_ms = 500, lsp_format = "prefer" },
                 cpp = { name = "clangd", timeout_ms = 500, lsp_format = "prefer" },
-                go = { "golines", "goimports", "gofmt", "gofumpt", "gci" },
+                go = { "golines", "goimports", "gofmt", "gofumpt" },
                 json = { "jq" },
                 lua = { "stylua" },
                 python = { "isort", "black" },

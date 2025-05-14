18ce21f3f66048d940d39b2a84f70002224e0804
Kim Gert Nielsen <kn@insecurity.dk>
2025-05-14 08:50:51 +0200

    Cleanup and formatting

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,106 +1,88 @@
 DATA_PATH = vim.fn.stdpath("data")
 HOME_PATH = vim.env.HOME
 
-vim.g.personal_projects_dir = HOME_PATH .. "/src/private"
-vim.g.work_projects_dir = HOME_PATH .. "/git"
-
-local global = vim.g
-local option = vim.o
+local g = vim.g -- Global
+local o = vim.o -- Options
 local opt = vim.opt
-local buffer_option = vim.bo
-local window_option = vim.wo
+local bo = vim.bo -- Buffer option
+local wo = vim.wo -- Window option
 local indent = 2
 
 vim.lsp.set_log_level("OFF") -- turn complely off when not duing 'debug'
 
-global.mapleader = "," -- Set leader
-global.maplocalleader = "," -- Set local leader
-global.shada = "NONE"
+g.personal_projects_dir = HOME_PATH .. "/src/private"
+g.work_projects_dir = HOME_PATH .. "/git"
+g.mapleader = "," -- Set leader
+g.maplocalleader = "," -- Set local leader
+g.shada = "NONE"
+g.markdown_fenched_languages = { "lua", "c", "cpp", "go", "ruby" } -- Fix highligts within markdown files
+g.splitkeep = "screen" -- stabilize buffer content
 
-vim.opt.guicursor = "" -- Use fat cursor
+opt.guicursor = "" -- Use fat cursor
 -- opt.guicursor = "a-i-ci:ver30-iCursor-blinkwait300-blinkon200-blinkoff150"
 opt.shortmess:append("I") -- Turn off splash
 opt.shortmess:append("c") -- Avoid showing message extra message when using completion
 opt.shortmess:append("s") -- Don't give "search hit BOTTOM, continuing at TOP" or "search
+opt.formatoptions:append({ "n" }) -- Better indent
+opt.iskeyword:append("-") -- Add - to be part of word
+opt.nrformats:append("unsigned") -- Increment
+opt.spelllang = { "en", "da" }
+opt.shell = "zsh"
+opt.isfname:append("@-@") -- Include @ in filename
 
--- better indent
-opt.formatoptions:append({ "n" })
+o.cmdheight = 2 -- Set height to prevent 'press enter to continue'
+o.hidden = true -- Allow to switch buffer without saving
+o.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+o.jumpoptions = "view" -- save 'view' when jumping
+o.updatetime = 300 -- Faster completion
+o.showmode = false -- We don't need to see things like -- INSERT -- anymore
+o.ruler = true -- Make search act like search in modern browsers
+o.incsearch = true
+o.hlsearch = false -- No search highlight
+o.backup = false -- Don't use backup
+o.writebackup = false
+o.showmatch = true -- Show matching braces
+o.synmaxcol = 500 -- Stop syntax highlight on long lines
+o.wrap = false -- Don't wrap lines
+o.textwidth = 0 -- Don't add newline after 80 chars
+o.scrolloff = 10 -- Always keep 10 lines visible
+o.errorbells = false -- Disable error bells
+o.showcmd = false -- Don't show commands
+o.undodir = DATA_PATH .. "/undo" -- Save and set undo/redo levels
+o.undofile = true
+o.undolevels = 100
+o.undoreload = 100
+o.listchars = "tab:▸ ,trail:·,nbsp:␣,extends:❯,precedes:❮" -- Hidden chars
+-- o.clipboard = "unnamedplus"
+o.foldenable = false
+o.swapfile = true
+o.conceallevel = 0 -- so that `` is visible in markdown files
+o.splitbelow = true
+o.splitright = true
+o.mouse = ""
+o.pumheight = 10 -- Maximum number of entries in a popup
+o.winborder = "rounded" -- Give all floating windows a border
 
-option.cmdheight = 2 -- Set height to prevent 'press enter to continue'
-option.hidden = true -- Allow to switch buffer without saving
+wo.signcolumn = "yes" -- Always show signcolumn // number
+wo.number = true -- Show numbers
+wo.relativenumber = true -- Show relative numbers
+wo.signcolumn = "yes" -- Make room for gitsigns + numbers
+wo.list = true -- Show hidden chars
+wo.cursorline = true
 
-option.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
-
-option.updatetime = 300 -- Faster completion
-
-option.showmode = false -- We don't need to see things like -- INSERT -- anymore
-option.ruler = true -- Make search act like search in modern browsers
-option.incsearch = true
-option.hlsearch = false -- No search highlight
-option.backup = false -- Don't use backup
-option.writebackup = false
-
-option.showmatch = true -- Show matching braces
-option.synmaxcol = 500 -- Stop syntax highlight on long lines
-option.wrap = false -- Don't wrap lines
-option.textwidth = 0 -- Don't add newline after 80 chars
-option.scrolloff = 10 -- Always keep 10 lines visible
-option.errorbells = false -- Disable error bells
-option.showcmd = false -- Don't show commands
-
-option.undodir = DATA_PATH .. "/undo" -- Save and set undo/redo levels
-option.undofile = true
-option.undolevels = 100
-option.undoreload = 100
-option.listchars = "tab:▸ ,trail:·,nbsp:␣,extends:❯,precedes:❮" -- Hidden chars
--- option.clipboard = "unnamedplus"
-
-window_option.signcolumn = "yes" -- Always show signcolumn // number
-window_option.number = true -- Show numbers
-window_option.relativenumber = true -- Show relative numbers
-window_option.signcolumn = "yes" -- Make room for gitsigns + numbers
-window_option.list = true -- Show hidden chars
-
-buffer_option.tabstop = indent -- Default set to spaces
-buffer_option.softtabstop = indent
-buffer_option.shiftwidth = indent
-buffer_option.expandtab = true
-buffer_option.autoindent = true
-buffer_option.smartindent = true
-buffer_option.scrollback = 10000
+bo.tabstop = indent -- Default set to spaces
+bo.softtabstop = indent
+bo.shiftwidth = indent
+bo.expandtab = true
+bo.autoindent = true
+bo.smartindent = true
+bo.scrollback = 10000
 
 -- Disable healthcheck
-global.loaded_perl_provider = 0
-global.loaded_python_provider = 0
-global.loaded_python3_provider = 0
-global.loaded_ruby_provider = 0
-global.loaded_node_provider = 0
-
-option.swapfile = true
-option.conceallevel = 0 -- so that `` is visible in markdown files
-
-option.splitbelow = true
-option.splitright = true
-
-option.mouse = ""
-window_option.cursorline = true
-
--- Include @ in filename
-opt.isfname:append("@-@")
-
-option.pumheight = 10 -- Maximum number of entries in a popup
-
--- Fix highligts within markdown files
-global.markdown_fenched_languages = { "lua", "c", "cpp", "go", "ruby" }
-
--- stabilize buffer content
-global.splitkeep = "screen"
-
-opt.spelllang = { "en", "da" }
-
-opt.shell = "zsh"
+g.loaded_perl_provider = 0
+g.loaded_python_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_ruby_provider = 0
+g.loaded_node_provider = 0
 
 vim.cmd("let g:netrw_liststyle = 3")
-
--- Give all floating windows a border
-option.winborder = "rounded"

9f85e6f1d7a50c8312ac188bed08dcf092dd0156
Kim Gert Nielsen <kn@insecurity.dk>
2025-05-15 08:16:42 +0200

    Clean up unused shortcuts

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,7 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
+    cmd = { "Gitsigns" },
     opts = {
         signs = {
             add = { text = "│" },
@@ -22,14 +23,13 @@ return {
             map("n", "[h", gs.prev_hunk, "Prev Hunk")
             map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
             map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+
             map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
             map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
             map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
             map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
             map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-            map("n", "<leader>ghd", gs.diffthis, "Diff This")
-            map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-            map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+            map("n", "<leader>ght", function() gs.toggle_current_line_blame() end, "Toggle line blame")
         end,
     },
 }

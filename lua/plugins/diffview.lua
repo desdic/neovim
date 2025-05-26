c2ae40623093720e43193592441b1573bbae99bc
Kim Gert Nielsen <kn@insecurity.dk>
2025-05-26 07:18:15 +0200

    use snacks for indent, remove diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,5 +1,6 @@
 return {
     "sindrets/diffview.nvim",
+    enabled = false,
     cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
     keys = {
         { "<leader>dv", "<cmd>DiffviewOpen<CR>", desc = "DiffView" },
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -52,6 +52,21 @@ return {
         bigfile = { enabled = true },
         statuscolumn = { enabled = true },
         quickfile = { enabled = true },
+        indent = {
+            enabled = true,
+            hl = {
+                "SnacksIndent1",
+                "SnacksIndent2",
+                "SnacksIndent3",
+                "SnacksIndent4",
+                "SnacksIndent5",
+                "SnacksIndent6",
+                "SnacksIndent7",
+                "SnacksIndent8",
+            },
+            animate = { enabled = false },
+            scope = { enabled = false },
+        },
     },
     keys = {
         {

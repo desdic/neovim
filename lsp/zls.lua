6668038c47b212caf67e2f3e8080bb086bee1144
Kim Gert Nielsen <kn@insecurity.dk>
2025-08-13 07:54:15 +0200

    Update zls

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -1,10 +1,22 @@
 return {
     cmd = { "zls" },
-    on_new_config = function(new_config, new_root_dir)
-        if vim.fn.filereadable(vim.fs.joinpath(new_root_dir, "zls.json")) ~= 0 then
-            new_config.cmd = { "zls", "--config-path", "zls.json" }
-        end
-    end,
     filetypes = { "zig", "zir" },
+    workspace_required = false,
     root_markers = { "zls.json", "build.zig", ".git" },
+    single_file_support = true,
+    settings = {
+        zls = {
+            -- Whether to enable build-on-save diagnostics
+            --
+            -- Further information about build-on save:
+            -- https://zigtools.org/zls/guides/build-on-save/
+            enable_build_on_save = true,
+
+            -- Neovim already provides basic syntax highlighting
+            semantic_tokens = "partial",
+
+            -- omit the following line if `zig` is in your PATH
+            zig_exe_path = "/sbin/zig",
+        },
+    },
 }

e1bbe8ee15f5cb5e794435449a6e3534596a6e18
Kim Gert Nielsen <kn@insecurity.dk>
2025-08-20 11:05:20 +0200

    Accept on CR + noselect

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -14,12 +14,13 @@ return {
     event = "InsertEnter",
     opts = {
         keymap = {
-            -- ["<CR>"] = { "accept", "fallback" },
+            ["<CR>"] = { "accept", "fallback" },
             ["<C-\\>"] = { "hide", "fallback" },
             ["<C-n>"] = { "select_next", "snippet_forward", "fallback" },
             ["<C-p>"] = { "select_prev" },
             ["<C-b>"] = { "scroll_documentation_up", "fallback" },
             ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+            ["<C-e>"] = false, -- disable
         },
         -- don't use in gitcommits or prompts
         enabled = function()
@@ -29,7 +30,7 @@ return {
         end,
         completion = {
             list = {
-                selection = { preselect = true, auto_insert = true },
+                selection = { preselect = false, auto_insert = true },
                 max_items = 10,
             },
 

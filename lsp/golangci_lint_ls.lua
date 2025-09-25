427a7499766f7bd571819e76e82f76d14363a3b3
Kim Gert Nielsen <kn@insecurity.dk>
2025-09-25 07:58:25 +0200

    Always use the same config for gilangci

--- lsp/golangci_lint_ls.lua
+++ lsp/golangci_lint_ls.lua
@@ -2,15 +2,13 @@ return {
     cmd = { "golangci-lint-langserver" },
     filetypes = { "go", "gomod" },
     init_options = {
-        command = { "golangci-lint", "run", "--output.json.path=stdout", "--show-stats=false" },
-    },
-    root_markers = {
-        ".golangci.yml",
-        ".golangci.yaml",
-        ".golangci.toml",
-        ".golangci.json",
-        "go.work",
-        "go.mod",
-        ".git",
+        command = {
+            "golangci-lint",
+            "run",
+            "--output.json.path=stdout",
+            "--show-stats=false",
+            "--concurrency=8",
+            "--config=~/.config/golangci/golangci.yaml",
+        },
     },
 }

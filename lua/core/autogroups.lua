b9bb67dd5671c63989a0030055f38292ed7ae733
Kim Gert Nielsen <kn@insecurity.dk>
2025-12-02 08:16:40 +0100

    Remove command never used

--- lua/core/autogroups.lua
+++ lua/core/autogroups.lua
@@ -191,8 +191,3 @@ vim.api.nvim_create_autocmd("VimResized", {
         vim.cmd("wincmd =")
     end,
 })
-
-vim.api.nvim_create_user_command("ToggleFormat", function()
-    vim.g.autoformat = not vim.g.autoformat
-    vim.notify(string.format("%s formatting...", vim.g.autoformat and "Enabling" or "Disabling"), vim.log.levels.INFO)
-end, { desc = "Toggle conform.nvim auto-formatting", nargs = 0 })

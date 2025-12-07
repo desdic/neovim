b495f50f26d63a4f33f1490cf118100f944cd747
Kim Gert Nielsen <kn@insecurity.dk>
2025-12-07 20:31:01 +0100

    add scratch buffer

--- lua/core/autogroups.lua
+++ lua/core/autogroups.lua
@@ -191,3 +191,17 @@ vim.api.nvim_create_autocmd("VimResized", {
         vim.cmd("wincmd =")
     end,
 })
+
+vim.api.nvim_create_user_command("Scratch", function()
+    vim.cmd("bel new")
+    local buf = vim.api.nvim_get_current_buf()
+    for name, value in pairs({
+        filetype = "scratch",
+        buftype = "nofile",
+        bufhidden = "wipe",
+        swapfile = false,
+        modifiable = true,
+    }) do
+        vim.api.nvim_set_option_value(name, value, { buf = buf })
+    end
+end, { desc = "Open a scratch buffer", nargs = 0 })

41db988cef7c3f1ec541e52ac2d9fc1210618b7e
Kim Gert Nielsen <kn@insecurity.dk>
2026-01-21 07:14:08 +0100

    Add markers for ruff

--- lsp/ruff.lua
+++ lsp/ruff.lua
@@ -1,6 +1,6 @@
 return {
     cmd = { "ruff", "server" },
     filetypes = { "python" },
-    root_markers = { "setup.py", "ruff.toml", ".ruff.toml" },
+    root_markers = { "setup.py", "ruff.toml", ".ruff.toml", "pyproject.toml", "requirements.txt" },
     settings = {},
 }

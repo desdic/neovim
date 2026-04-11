e423992bc6c4122be79c54a42023c809c9cd459f
Kim Gert Nielsen <kn@insecurity.dk>
2026-04-11 08:28:50 +0200

    use default settings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,34 +3,5 @@ vim.pack.add({
 }, { confirm = false })
 
 vim.defer_fn(function()
-    require("gitsigns").setup({
-        signs = {
-            add = { text = "│" },
-            change = { text = "│" },
-            delete = { text = "" },
-            topdelete = { text = "" },
-            changedelete = { text = "│" },
-            untracked = { text = "│" },
-        },
-        on_attach = function(buffer)
-            local gs = package.loaded.gitsigns
-
-            local function map(mode, l, r, desc)
-                vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-            end
-
-            -- stylua: ignore start
-            map("n", "]h", gs.next_hunk, "Next Hunk")
-            map("n", "[h", gs.prev_hunk, "Prev Hunk")
-            map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-            map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-
-            map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-            map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-            map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-            map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
-            map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-            map("n", "<leader>ght", function() gs.toggle_current_line_blame() end, "Toggle line blame")
-        end,
-    })
+    require("gitsigns").setup({})
 end, 100)

d620b6f26862a3305907ddecbdb95fb17bbdb588
Kim Gert Nielsen <kn@insecurity.dk>
2026-04-21 10:04:08 +0200

    lazy load go
    mostly because its really annoying to see a message that I don't have
    nvim-treesitter installed on every file so now its just go, gosum and
    gomod

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,24 +1,36 @@
-vim.defer_fn(function()
-    vim.pack.add({
-        { src = "https://github.com/ray-x/go.nvim" },
-        { src = "https://github.com/ray-x/guihua.lua" },
-    }, { confirm = false })
+vim.pack.add({
+    { src = "https://github.com/ray-x/go.nvim" },
+    { src = "https://github.com/ray-x/guihua.lua" },
+}, { confirm = false })
 
-    require("go").setup({
-        lsp_inlay_hints = { enable = false },
-        diagnostic = false,
-    })
+vim.api.nvim_create_autocmd({ "PackChanged" }, {
+    group = vim.api.nvim_create_augroup("XRayGoUpdated", { clear = true }),
+    callback = function(args)
+        local spec = args.data.spec
+        if spec and spec.name == "go.nvim" and args.data.kind == "update" then
+            vim.notify("x-ray/go was updated, running update_all_sync", vim.log.levels.INFO)
+            vim.schedule(function()
+                require("go.install").update_all_sync()
+            end)
+        end
+    end,
+})
 
-    vim.api.nvim_create_autocmd({ "PackChanged" }, {
-        group = vim.api.nvim_create_augroup("XRayGoUpdated", { clear = true }),
-        callback = function(args)
-            local spec = args.data.spec
-            if spec and spec.name == "go.nvim" and args.data.kind == "update" then
-                vim.notify("x-ray/go was updated, running update_all_sync", vim.log.levels.INFO)
-                vim.schedule(function()
-                    require("go.install").update_all_sync()
-                end)
-            end
-        end,
-    })
-end, 500)
+local go_load_group = vim.api.nvim_create_augroup("LazyLoadGo", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = go_load_group,
+    pattern = { "go", "gomod", "gosum" },
+    callback = function()
+        -- Guard clause: if already loaded, don't run again
+        if vim.g.go_nvim_loaded then
+            return
+        end
+
+        require("go").setup({
+            lsp_inlay_hints = { enable = false },
+            diagnostic = false,
+        })
+
+        vim.g.go_nvim_loaded = true
+    end,
+})

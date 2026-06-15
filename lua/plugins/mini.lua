9ee0ffd9fa2b0c659e1f5337cf7a61974caa50f6
Kim Gert Nielsen <kn@insecurity.dk>
2026-06-15 06:52:12 +0200

    Add mini test but don't run setup
    used to test plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,5 @@
 vim.pack.add({
     { src = "https://github.com/nvim-mini/mini.icons" },
+    { src = "https://github.com/nvim-mini/mini.test" },
 }, { confirm = false })
 require("mini.icons").setup({})

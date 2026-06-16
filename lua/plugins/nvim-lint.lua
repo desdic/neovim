3bdbdcb18c480a6e9be5fc3e4e3a487e57658a54
Kim Gert Nielsen <kn@insecurity.dk>
2026-06-16 11:51:01 +0200

    lazy load linter

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,46 +1,40 @@
-vim.defer_fn(function()
-    vim.pack.add({
-        { src = "https://github.com/mfussenegger/nvim-lint" },
-    }, { confirm = false })
+vim.pack.add({
+    { src = "https://github.com/mfussenegger/nvim-lint", load = false },
+}, { confirm = false })
 
-    local lint = require("lint")
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "go", "gomod", "gosum", "c", "cpp", "json", "python", "cmake" },
+    callback = function(event)
+        if vim.g.lint_nvim_loaded then
+            return
+        end
 
-    -- Override options since latest golangcilint has changed options
-    local golangcilint = lint.linters.golangcilint
-    golangcilint.args = {
-        "run",
-        "--output.json.path",
-        "stdout",
-        "--issues-exit-code=0",
-        "--show-stats=false",
-        "--allow-parallel-runners",
-        "--concurrency=8",
-        "--config",
-        "~/.config/golangci/golangci.yaml",
-        function()
-            return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
-        end,
-    }
+        vim.cmd("packadd nvim-lint")
 
-    lint.linters_by_ft = {
-        c = { "flawfinder" },
-        cmake = { "cmakelint" },
-        -- go = { "golangcilint" },
-        json = { "jq" },
-        python = { "ruff", "pylint" },
-        sh = { "shellcheck" },
-    }
+        local lint = require("lint")
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+        lint.linters_by_ft = {
+            c = { "flawfinder" },
+            cmake = { "cmakelint" },
+            go = { "golangcilint" },
+            json = { "jq" },
+            python = { "ruff", "pylint" },
+            sh = { "shellcheck" },
+        }
 
-    vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
-        group = lint_augroup,
-        callback = function()
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+        vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
+            group = lint_augroup,
+            callback = function()
+                lint.try_lint()
+            end,
+        })
+
+        vim.keymap.set("n", "<leader>lf", function()
             lint.try_lint()
-        end,
-    })
+        end, { desc = "Trigger linting for current file" })
 
-    vim.keymap.set("n", "<leader>lf", function()
-        lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-end, 500)
+        vim.g.lint_nvim_loaded = true
+    end,
+})

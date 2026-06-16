ecce0b2e681a654a29adeefd29bb778ba92c6e6f
Kim Gert Nielsen <kn@insecurity.dk>
2026-06-16 13:47:23 +0200

    lazyload conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,29 @@
-vim.defer_fn(function()
-    vim.pack.add({
-        { src = "https://github.com/stevearc/conform.nvim" },
-    }, { confirm = false })
+vim.pack.add({
+    { src = "https://github.com/stevearc/conform.nvim", load = false },
+}, { confirm = false })
 
-    require("conform").setup({
-        formatters_by_ft = {
-            c = { name = "clangd", timeout_ms = 500, lsp_format = "prefer" },
-            cpp = { name = "clangd", timeout_ms = 500, lsp_format = "prefer" },
-            go = { "goimports", "gofmt", "gofumpt" },
-            json = { "jq" },
-            lua = { "stylua" },
-            python = { "isort", "black" },
-            rust = { "rustfmt", lsp_format = "fallback" },
-            sh = { "shfmt" },
-            zig = { "zigfmt", lsp_format = "fallback" },
-            glsl = { "clang-format" },
-        },
-    })
-end, 500)
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "go", "gomod", "gosum", "c", "cpp", "json", "python", "cmake", "rust", "lua", "glsl" },
+    callback = function(event)
+        if vim.g.conform_nvim_loaded then
+            return
+        end
+
+        vim.cmd("packadd nvim-lint")
+        require("conform").setup({
+            formatters_by_ft = {
+                c = { name = "clangd", timeout_ms = 500, lsp_format = "prefer" },
+                cpp = { name = "clangd", timeout_ms = 500, lsp_format = "prefer" },
+                go = { "goimports", "gofmt", "gofumpt" },
+                json = { "jq" },
+                lua = { "stylua" },
+                python = { "isort", "black" },
+                rust = { "rustfmt", lsp_format = "fallback" },
+                sh = { "shfmt" },
+                glsl = { "clang-format" },
+            },
+        })
+
+        vim.g.conform_nvim_loaded = true
+    end,
+})
